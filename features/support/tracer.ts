f3fa4abdea5a6d7c7fd1c3349c083e25e1d4d21a
Jiri Kuncar <jiri.kuncar@gmail.com>
2021-02-19 16:54:11 +0100

    add x-datadog-* headers

--- features/support/tracer.ts
+++ features/support/tracer.ts
@@ -73,4 +73,26 @@ PickleRunner.prototype.run = handleRun;
 PickleRunner.prototype.runStep = handleRunStep;
 // TODO store the original methods
 
+const v1 = require('../../packages/datadog-api-client-v1/http/isomorphic-fetch').IsomorphicFetchHttpLibrary;
+const v1Send = v1.prototype.send;
+const v2 = require('../../packages/datadog-api-client-v2/http/isomorphic-fetch').IsomorphicFetchHttpLibrary;
+const v2Send = v2.prototype.send;
+
+function wrap(method: any) {
+  function send(request: any): any {
+    let span = tracer.scope().active();
+    if (span !== null) {
+      let spanId = span.context().toSpanId();
+      let traceId = span.context().toTraceId();
+      request.setHeaderParam('x-datadog-parent-id', spanId);
+      request.setHeaderParam('x-datadog-trace-id', traceId);
+    }
+    return method(request)
+  }
+  return send;
+}
+
+v1.prototype.send = wrap(v1Send);
+v2.prototype.send = wrap(v2Send);
+
 export default tracer;
