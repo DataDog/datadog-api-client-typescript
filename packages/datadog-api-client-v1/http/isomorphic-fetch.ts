bf6d76215e28a28fb36a4a2f143097e44d530b32
Jiri Kuncar <jiri.kuncar@gmail.com>
2021-02-19 14:41:16 +0100

    Revert "Fix distributed traces"
    This reverts commit 95fdf36c4b988d2ff39a94376dcd71be709bbee7.

--- packages/datadog-api-client-v1/http/isomorphic-fetch.ts
+++ packages/datadog-api-client-v1/http/isomorphic-fetch.ts
@@ -1,13 +1,12 @@
 import {HttpLibrary, RequestContext, ResponseContext} from './http';
 import { from, Observable } from '../rxjsStub';
-// import fetch from "node-fetch";
+import fetch from "node-fetch";
 
 export class IsomorphicFetchHttpLibrary implements HttpLibrary {
 
     public send(request: RequestContext): Observable<ResponseContext> {
         let method = request.getHttpMethod().toString();
         let body = request.getBody();
-        const fetch = require('node-fetch') 
 
         const resultPromise = fetch(request.getUrl(), {
             method: method,
