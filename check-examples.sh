a6049ade0beeb7dec52e0ee2b8af20408e563290
NouemanKHAL <noueman.khal@gmail.com>
2021-07-23 13:56:39 +0200

    add outDir to examples tsconfig

--- check-examples.sh
+++ check-examples.sh
@@ -15,7 +15,7 @@ find examples/$1 -type f -name "*.ts" | xargs sed -i.bak 's_@datadog/datadog-api
 
 touch examples/$1/tsconfig.json
 
-echo '{"compilerOptions":{"strict":true,"target":"es5","module":"commonjs","moduleResolution":"node","declaration":true,"esModuleInterop":true,"resolveJsonModule":true,"noImplicitAny":true,"noImplicitThis":true,"noUnusedLocals":false,"noUnusedParameters":false,"noImplicitReturns":true,"noFallthroughCasesInSwitch":true,"sourceMap":true,"noLib":false,"lib":["es5","es6","es7"]},"exclude":["dist","features","node_modules"],"include":["./**/*.ts"]}' > examples/$1/tsconfig.json
+echo '{"compilerOptions":{"strict":true,"target":"es5","module":"commonjs","moduleResolution":"node","declaration":true,"esModuleInterop":true,"resolveJsonModule":true,"noImplicitAny":true,"noImplicitThis":true,"noUnusedLocals":false,"noUnusedParameters":false,"noImplicitReturns":true,"noFallthroughCasesInSwitch":true,"sourceMap":true,"noLib":false,"lib":["es5","es6","es7"],"outDir":"../../dist"},"exclude":["dist","features","node_modules"],"include":["./**/*.ts"]}' > examples/$1/tsconfig.json
 
 yarn tsc --build examples/$1
 
