bf6257ee3d7b24564c6c9d471c714a0783888d8b
NouemanKHAL <noueman.khal@gmail.com>
2021-11-24 15:55:05 +0100

    use logger

--- features/support/hooks.ts
+++ features/support/hooks.ts
@@ -2,6 +2,8 @@ import { AfterAll, Before } from "@cucumber/cucumber";
 import { ITestCaseHookParameter } from "@cucumber/cucumber/lib/support_code_library_builder/types";
 
 import { World } from "./world";
+import log from "loglevel";
+const logger = log.getLogger("testing")
 
 Before({ tags: "@skip or @skip-typescript" }, function () {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -34,8 +36,8 @@ AfterAll( function (this: World) {
   const dd_service = process.env.DD_SERVICE;
   const ci_pipeline_id = process.env.GITHUB_RUN_ID;
   if (dd_service !== undefined && ci_pipeline_id !== undefined) {
-    console.log("\nTest reports:\n")
-    console.log("* View test APM traces and detailed time reports on Datadog (can take a few minutes to become available):")
-    console.log(`* https://app.datadoghq.com/ci/test-runs?query=%40test.service%3A${dd_service}%20%40ci.pipeline.id%3A${ci_pipeline_id}&index=citest\n`)
+    logger.info("\nTest reports:\n")
+    logger.info("* View test APM traces and detailed time reports on Datadog (can take a few minutes to become available):")
+    logger.info(`* https://app.datadoghq.com/ci/test-runs?query=%40test.service%3A${dd_service}%20%40ci.pipeline.id%3A${ci_pipeline_id}&index=citest\n`)
   }
 });
