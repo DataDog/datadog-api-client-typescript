5735a906dc67655786cc9cd5d1a8fe6e7348020e
Jiri Kuncar <jiri.kuncar@datadoghq.com>
2021-12-13 18:10:35 +0100

    Ensure `test.codeowners` tags on skipped tests (#420)

--- features/support/hooks.ts
+++ features/support/hooks.ts
@@ -4,6 +4,24 @@ import tracer from "./tracer";
 
 import { World } from "./world";
 
+Before(function (
+  this: World,
+  { gherkinDocument, pickle }: ITestCaseHookParameter
+) {
+  const parts = gherkinDocument.uri?.split("/") as string[];
+  this.apiVersion = parts[parts.length - 2];
+  
+  const span = tracer.scope().active() as any;
+  if (span !== null) {
+    const codeowners = JSON.stringify(
+      pickle.tags
+      .filter((tag) => tag.name.startsWith("@team:"))
+      .map((tag) => "@" + tag.name.substring("@team:".length))
+    );
+    span.setTag("test.codeowners", codeowners);
+  }
+});
+
 Before({ tags: "@skip or @skip-typescript" }, function () {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return "skipped" as any;
@@ -20,23 +38,5 @@ Before({ tags: "@replay-only" }, function () {
   if ("RECORD" in process.env && process.env["RECORD"] != "false") {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     return "skipped" as any;
-  }
-});
-
-Before(function (
-  this: World,
-  { gherkinDocument, pickle }: ITestCaseHookParameter
-) {
-  const parts = gherkinDocument.uri?.split("/") as string[];
-  this.apiVersion = parts[parts.length - 2];
-  
-  const span = tracer.scope().active() as any;
-  if (span !== null) {
-    const codeowners = JSON.stringify(
-      pickle.tags
-      .filter((tag) => tag.name.startsWith("@team:"))
-      .map((tag) => "@" + tag.name.substring("@team:".length))
-    );
-    span.setTag("test.codeowners", codeowners);
   }
 });
