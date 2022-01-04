a58009fb80d573b6c56eabc27ad7c9c58c0d25a4
ci.datadog-api-spec <packages@datadoghq.com>
2022-01-04 09:54:58 +0000

    Regenerate client from commit 184be3f of spec repo

---
+++ examples/v1/synthetics/CreateSyntheticsAPITest_1279271422.ts
@@ -0,0 +1,77 @@
+/**
+ * Create an API test with multi subtype returns "OK - Returns the created test details." response
+ */
+
+import { v1 } from "@datadog/datadog-api-client";
+
+const configuration = v1.createConfiguration();
+const apiInstance = new v1.SyntheticsApi(configuration);
+
+let params: v1.SyntheticsApiCreateSyntheticsAPITestRequest = {
+  body: {
+    config: {
+      configVariables: [
+        {
+          example: "content-type",
+          name: "PROPERTY",
+          pattern: "content-type",
+          type: "text",
+        },
+      ],
+      steps: [
+        {
+          allowFailure: true,
+          assertions: [
+            {
+              operator: "is",
+              type: "statusCode",
+              target: 200,
+            },
+          ],
+          isCritical: true,
+          name: "request is sent",
+          request: {
+            method: "GET",
+            timeout: 10,
+            url: "https://datadoghq.com",
+          },
+          retry: {
+            count: 5,
+            interval: 1000,
+          },
+          subtype: "http",
+        },
+      ],
+    },
+    locations: ["aws:us-east-2"],
+    message: "BDD test payload: synthetics_api_test_multi_step_payload.json",
+    name: "Example-Create_an_API_test_with_multi_subtype_returns_OK_Returns_the_created_test_details_response",
+    options: {
+      acceptSelfSigned: false,
+      allowInsecure: true,
+      followRedirects: true,
+      minFailureDuration: 10,
+      minLocationFailed: 1,
+      monitorName:
+        "Example-Create_an_API_test_with_multi_subtype_returns_OK_Returns_the_created_test_details_response",
+      monitorPriority: 5,
+      retry: {
+        count: 3,
+        interval: 1000,
+      },
+      tickEvery: 60,
+    },
+    subtype: "multi",
+    tags: ["testing:api"],
+    type: "api",
+  },
+};
+
+apiInstance
+  .createSyntheticsAPITest(params)
+  .then((data: any) => {
+    console.log(
+      "API called successfully. Returned data: " + JSON.stringify(data)
+    );
+  })
+  .catch((error: any) => console.error(error));
