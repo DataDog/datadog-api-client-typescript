56b9ffdab022140445284208de9f68db11792d88
Thomas Hervé <thomas.herve@datadoghq.com>
2022-01-24 10:10:13 +0100

    Handle lack of process variable (#494)
    * Handle lack of process variable
    
    To prepare for non-node environments, let's check if the process
    variable exists.
    
    * Handle the logger

--- index.ts
+++ index.ts
@@ -4,6 +4,6 @@ export * as v2 from "./packages/datadog-api-client-v2";
 import log from "loglevel";
 
 const logger = log.noConflict();
-logger.setLevel(process.env.DEBUG ? logger.levels.DEBUG : logger.levels.INFO);
+logger.setLevel((process !== undefined && process.env.DEBUG) ? logger.levels.DEBUG : logger.levels.INFO);
 
 export { logger };
