1627055a98ce57bafbf237e3bd7cdfbeacd9c24a
ci.datadog-api-spec <packages@datadoghq.com>
2022-03-01 10:13:16 +0000

    Regenerate client from commit 7fe3e9a of spec repo

---
+++ examples/v1/dashboards/DeleteDashboard.ts
@@ -0,0 +1,24 @@
+/**
+ * Delete a dashboard returns "OK" response
+ */
+
+import { v1 } from "@datadog/datadog-api-client";
+
+const configuration = v1.createConfiguration();
+const apiInstance = new v1.DashboardsApi(configuration);
+
+// there is a valid "dashboard" in the system
+let DASHBOARD_ID = process.env.DASHBOARD_ID as string;
+
+let params: v1.DashboardsApiDeleteDashboardRequest = {
+  dashboardId: DASHBOARD_ID,
+};
+
+apiInstance
+  .deleteDashboard(params)
+  .then((data: v1.DashboardDeleteResponse) => {
+    console.log(
+      "API called successfully. Returned data: " + JSON.stringify(data)
+    );
+  })
+  .catch((error: any) => console.error(error));
