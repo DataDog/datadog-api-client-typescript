54e42ad7edbb0fb7ec985a299ede75bcddb35319
ci.datadog-api-spec <packages@datadoghq.com>
2022-07-01 08:42:45 +0000

    Regenerate client from commit be2f0c5e of spec repo

--- packages/datadog-api-client-v1/models/UsageBillableSummaryKeys.ts
+++ packages/datadog-api-client-v1/models/UsageBillableSummaryKeys.ts
@@ -14,6 +14,14 @@ export class UsageBillableSummaryKeys {
   /**
    * Response with properties for each aggregated usage type.
    */
+  "apmFargateAverage"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "apmFargateSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
   "apmHostSum"?: UsageBillableSummaryBody;
   /**
    * Response with properties for each aggregated usage type.
@@ -22,11 +30,127 @@ export class UsageBillableSummaryKeys {
   /**
    * Response with properties for each aggregated usage type.
    */
+  "apmProfilerHostSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "apmProfilerHostTop99p"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
   "apmTraceSearchSum"?: UsageBillableSummaryBody;
   /**
    * Response with properties for each aggregated usage type.
    */
+  "applicationSecurityHostSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "ciPipelineIndexedSpansSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "ciPipelineMaximum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "ciPipelineSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "ciTestIndexedSpansSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "ciTestingMaximum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "ciTestingSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "cspmContainerSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "cspmHostSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "cspmHostTop99p"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "customEventSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "cwsContainerSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "cwsHostSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "cwsHostTop99p"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "dbmHostSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "dbmHostTop99p"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "dbmNormalizedQueriesAverage"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "dbmNormalizedQueriesSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "fargateContainerApmAndProfilerAverage"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "fargateContainerApmAndProfilerSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
   "fargateContainerAverage"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "fargateContainerProfilerAverage"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "fargateContainerProfilerSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "fargateContainerSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "incidentManagementMaximum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "incidentManagementSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "infraAndApmHostSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "infraAndApmHostTop99p"?: UsageBillableSummaryBody;
   /**
    * Response with properties for each aggregated usage type.
    */
@@ -42,11 +166,31 @@ export class UsageBillableSummaryKeys {
   /**
    * Response with properties for each aggregated usage type.
    */
+  "ingestedSpansSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "ingestedTimeseriesAverage"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "ingestedTimeseriesSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "iotSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
   "iotTop99p"?: UsageBillableSummaryBody;
   /**
    * Response with properties for each aggregated usage type.
    */
   "lambdaFunctionAverage"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "lambdaFunctionSum"?: UsageBillableSummaryBody;
   /**
    * Response with properties for each aggregated usage type.
    */
@@ -59,6 +203,10 @@ export class UsageBillableSummaryKeys {
    * Response with properties for each aggregated usage type.
    */
   "logsIndexed30daySum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "logsIndexed360daySum"?: UsageBillableSummaryBody;
   /**
    * Response with properties for each aggregated usage type.
    */
@@ -94,6 +242,10 @@ export class UsageBillableSummaryKeys {
   /**
    * Response with properties for each aggregated usage type.
    */
+  "networkDeviceSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
   "networkDeviceTop99p"?: UsageBillableSummaryBody;
   /**
    * Response with properties for each aggregated usage type.
@@ -110,7 +262,19 @@ export class UsageBillableSummaryKeys {
   /**
    * Response with properties for each aggregated usage type.
    */
+  "observabilityPipelineSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "onlineArchiveSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
   "profContainerSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "profHostSum"?: UsageBillableSummaryBody;
   /**
    * Response with properties for each aggregated usage type.
    */
@@ -118,7 +282,23 @@ export class UsageBillableSummaryKeys {
   /**
    * Response with properties for each aggregated usage type.
    */
+  "rumLiteSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "rumReplaySum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
   "rumSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "rumUnitsSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "sensitiveDataScannerSum"?: UsageBillableSummaryBody;
   /**
    * Response with properties for each aggregated usage type.
    */
@@ -127,6 +307,10 @@ export class UsageBillableSummaryKeys {
    * Response with properties for each aggregated usage type.
    */
   "siemSum"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "standardTimeseriesAverage"?: UsageBillableSummaryBody;
   /**
    * Response with properties for each aggregated usage type.
    */
@@ -139,6 +323,10 @@ export class UsageBillableSummaryKeys {
    * Response with properties for each aggregated usage type.
    */
   "timeseriesAverage"?: UsageBillableSummaryBody;
+  /**
+   * Response with properties for each aggregated usage type.
+   */
+  "timeseriesSum"?: UsageBillableSummaryBody;
 
   /**
    * @ignore
@@ -149,6 +337,14 @@ export class UsageBillableSummaryKeys {
    * @ignore
    */
   static readonly attributeTypeMap: AttributeTypeMap = {
+    apmFargateAverage: {
+      baseName: "apm_fargate_average",
+      type: "UsageBillableSummaryBody",
+    },
+    apmFargateSum: {
+      baseName: "apm_fargate_sum",
+      type: "UsageBillableSummaryBody",
+    },
     apmHostSum: {
       baseName: "apm_host_sum",
       type: "UsageBillableSummaryBody",
@@ -157,12 +353,128 @@ export class UsageBillableSummaryKeys {
       baseName: "apm_host_top99p",
       type: "UsageBillableSummaryBody",
     },
+    apmProfilerHostSum: {
+      baseName: "apm_profiler_host_sum",
+      type: "UsageBillableSummaryBody",
+    },
+    apmProfilerHostTop99p: {
+      baseName: "apm_profiler_host_top99p",
+      type: "UsageBillableSummaryBody",
+    },
     apmTraceSearchSum: {
       baseName: "apm_trace_search_sum",
       type: "UsageBillableSummaryBody",
     },
+    applicationSecurityHostSum: {
+      baseName: "application_security_host_sum",
+      type: "UsageBillableSummaryBody",
+    },
+    ciPipelineIndexedSpansSum: {
+      baseName: "ci_pipeline_indexed_spans_sum",
+      type: "UsageBillableSummaryBody",
+    },
+    ciPipelineMaximum: {
+      baseName: "ci_pipeline_maximum",
+      type: "UsageBillableSummaryBody",
+    },
+    ciPipelineSum: {
+      baseName: "ci_pipeline_sum",
+      type: "UsageBillableSummaryBody",
+    },
+    ciTestIndexedSpansSum: {
+      baseName: "ci_test_indexed_spans_sum",
+      type: "UsageBillableSummaryBody",
+    },
+    ciTestingMaximum: {
+      baseName: "ci_testing_maximum",
+      type: "UsageBillableSummaryBody",
+    },
+    ciTestingSum: {
+      baseName: "ci_testing_sum",
+      type: "UsageBillableSummaryBody",
+    },
+    cspmContainerSum: {
+      baseName: "cspm_container_sum",
+      type: "UsageBillableSummaryBody",
+    },
+    cspmHostSum: {
+      baseName: "cspm_host_sum",
+      type: "UsageBillableSummaryBody",
+    },
+    cspmHostTop99p: {
+      baseName: "cspm_host_top99p",
+      type: "UsageBillableSummaryBody",
+    },
+    customEventSum: {
+      baseName: "custom_event_sum",
+      type: "UsageBillableSummaryBody",
+    },
+    cwsContainerSum: {
+      baseName: "cws_container_sum",
+      type: "UsageBillableSummaryBody",
+    },
+    cwsHostSum: {
+      baseName: "cws_host_sum",
+      type: "UsageBillableSummaryBody",
+    },
+    cwsHostTop99p: {
+      baseName: "cws_host_top99p",
+      type: "UsageBillableSummaryBody",
+    },
+    dbmHostSum: {
+      baseName: "dbm_host_sum",
+      type: "UsageBillableSummaryBody",
+    },
+    dbmHostTop99p: {
+      baseName: "dbm_host_top99p",
+      type: "UsageBillableSummaryBody",
+    },
+    dbmNormalizedQueriesAverage: {
+      baseName: "dbm_normalized_queries_average",
+      type: "UsageBillableSummaryBody",
+    },
+    dbmNormalizedQueriesSum: {
+      baseName: "dbm_normalized_queries_sum",
+      type: "UsageBillableSummaryBody",
+    },
+    fargateContainerApmAndProfilerAverage: {
+      baseName: "fargate_container_apm_and_profiler_average",
+      type: "UsageBillableSummaryBody",
+    },
+    fargateContainerApmAndProfilerSum: {
+      baseName: "fargate_container_apm_and_profiler_sum",
+      type: "UsageBillableSummaryBody",
+    },
     fargateContainerAverage: {
       baseName: "fargate_container_average",
+      type: "UsageBillableSummaryBody",
+    },
+    fargateContainerProfilerAverage: {
+      baseName: "fargate_container_profiler_average",
+      type: "UsageBillableSummaryBody",
+    },
+    fargateContainerProfilerSum: {
+      baseName: "fargate_container_profiler_sum",
+      type: "UsageBillableSummaryBody",
+    },
+    fargateContainerSum: {
+      baseName: "fargate_container_sum",
+      type: "UsageBillableSummaryBody",
+    },
+    incidentManagementMaximum: {
+      baseName: "incident_management_maximum",
+      type: "UsageBillableSummaryBody",
+    },
+    incidentManagementSum: {
+      baseName: "incident_management_sum",
+      type: "UsageBillableSummaryBody",
+    },
+    infraAndApmHostSum: {
+      baseName: "infra_and_apm_host_sum",
+      type: "UsageBillableSummaryBody",
+    },
+    infraAndApmHostTop99p: {
+      baseName: "infra_and_apm_host_top99p",
       type: "UsageBillableSummaryBody",
     },
     infraContainerSum: {
@@ -177,12 +489,32 @@ export class UsageBillableSummaryKeys {
       baseName: "infra_host_top99p",
       type: "UsageBillableSummaryBody",
     },
+    ingestedSpansSum: {
+      baseName: "ingested_spans_sum",
+      type: "UsageBillableSummaryBody",
+    },
+    ingestedTimeseriesAverage: {
+      baseName: "ingested_timeseries_average",
+      type: "UsageBillableSummaryBody",
+    },
+    ingestedTimeseriesSum: {
+      baseName: "ingested_timeseries_sum",
+      type: "UsageBillableSummaryBody",
+    },
+    iotSum: {
+      baseName: "iot_sum",
+      type: "UsageBillableSummaryBody",
+    },
     iotTop99p: {
       baseName: "iot_top99p",
       type: "UsageBillableSummaryBody",
     },
     lambdaFunctionAverage: {
       baseName: "lambda_function_average",
+      type: "UsageBillableSummaryBody",
+    },
+    lambdaFunctionSum: {
+      baseName: "lambda_function_sum",
       type: "UsageBillableSummaryBody",
     },
     logsIndexed15daySum: {
@@ -195,6 +527,10 @@ export class UsageBillableSummaryKeys {
     },
     logsIndexed30daySum: {
       baseName: "logs_indexed_30day_sum",
+      type: "UsageBillableSummaryBody",
+    },
+    logsIndexed360daySum: {
+      baseName: "logs_indexed_360day_sum",
       type: "UsageBillableSummaryBody",
     },
     logsIndexed3daySum: {
@@ -229,6 +565,10 @@ export class UsageBillableSummaryKeys {
       baseName: "logs_ingested_sum",
       type: "UsageBillableSummaryBody",
     },
+    networkDeviceSum: {
+      baseName: "network_device_sum",
+      type: "UsageBillableSummaryBody",
+    },
     networkDeviceTop99p: {
       baseName: "network_device_top99p",
       type: "UsageBillableSummaryBody",
@@ -245,16 +585,44 @@ export class UsageBillableSummaryKeys {
       baseName: "npm_host_top99p",
       type: "UsageBillableSummaryBody",
     },
+    observabilityPipelineSum: {
+      baseName: "observability_pipeline_sum",
+      type: "UsageBillableSummaryBody",
+    },
+    onlineArchiveSum: {
+      baseName: "online_archive_sum",
+      type: "UsageBillableSummaryBody",
+    },
     profContainerSum: {
       baseName: "prof_container_sum",
+      type: "UsageBillableSummaryBody",
+    },
+    profHostSum: {
+      baseName: "prof_host_sum",
       type: "UsageBillableSummaryBody",
     },
     profHostTop99p: {
       baseName: "prof_host_top99p",
       type: "UsageBillableSummaryBody",
     },
+    rumLiteSum: {
+      baseName: "rum_lite_sum",
+      type: "UsageBillableSummaryBody",
+    },
+    rumReplaySum: {
+      baseName: "rum_replay_sum",
+      type: "UsageBillableSummaryBody",
+    },
     rumSum: {
       baseName: "rum_sum",
+      type: "UsageBillableSummaryBody",
+    },
+    rumUnitsSum: {
+      baseName: "rum_units_sum",
+      type: "UsageBillableSummaryBody",
+    },
+    sensitiveDataScannerSum: {
+      baseName: "sensitive_data_scanner_sum",
       type: "UsageBillableSummaryBody",
     },
     serverlessInvocationSum: {
@@ -263,6 +631,10 @@ export class UsageBillableSummaryKeys {
     },
     siemSum: {
       baseName: "siem_sum",
+      type: "UsageBillableSummaryBody",
+    },
+    standardTimeseriesAverage: {
+      baseName: "standard_timeseries_average",
       type: "UsageBillableSummaryBody",
     },
     syntheticsApiTestsSum: {
@@ -275,6 +647,10 @@ export class UsageBillableSummaryKeys {
     },
     timeseriesAverage: {
       baseName: "timeseries_average",
+      type: "UsageBillableSummaryBody",
+    },
+    timeseriesSum: {
+      baseName: "timeseries_sum",
       type: "UsageBillableSummaryBody",
     },
   };
