f91c36235550513e87e83c28c86f1013caad0c38
Sherzod K <sherzod.karimov@datadoghq.com>
2022-07-26 16:05:46 -0400

    generate pagination examples

---
+++ examples/v2/events/SearchEvents_3856995058.ts
@@ -0,0 +1,32 @@
+/**
+ * Search events returns "OK" response with pagination
+ */
+
+import { client, v2 } from "@datadog/datadog-api-client";
+
+const configuration = client.createConfiguration();
+configuration.unstableOperations["searchEvents"] = true;
+const apiInstance = new v2.EventsApi(configuration);
+
+const params: v2.EventsApiSearchEventsRequest = {
+  body: {
+    filter: {
+      from: "now-15m",
+      to: "now",
+    },
+    options: {
+      timezone: "GMT",
+    },
+    page: {
+      limit: 2,
+    },
+    sort: "timestamp",
+  },
+};
+try {
+  for await (const item of apiInstance.searchEventsWithPagination(params)) {
+    console.log(item);
+  }
+} catch (error) {
+  console.error(error);
+}
---
+++ examples/v2/incidents/ListIncidents_2665616954.ts
@@ -0,0 +1,20 @@
+/**
+ * Get a list of incidents returns "OK" response with pagination
+ */
+
+import { client, v2 } from "@datadog/datadog-api-client";
+
+const configuration = client.createConfiguration();
+configuration.unstableOperations["listIncidents"] = true;
+const apiInstance = new v2.IncidentsApi(configuration);
+
+const params: v2.IncidentsApiListIncidentsRequest = {
+  pageSize: 2,
+};
+try {
+  for await (const item of apiInstance.listIncidentsWithPagination(params)) {
+    console.log(item);
+  }
+} catch (error) {
+  console.error(error);
+}
