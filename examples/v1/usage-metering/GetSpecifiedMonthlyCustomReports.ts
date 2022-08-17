fdffcbeff141de59adf13e887ab81dbd3df46de3
ci.datadog-api-spec <packages@datadoghq.com>
2022-08-17 14:33:48 +0000

    Regenerate client from commit a957cd49 of spec repo

--- examples/v1/usage-metering/GetSpecifiedMonthlyCustomReports.ts
+++ examples/v1/usage-metering/GetSpecifiedMonthlyCustomReports.ts
@@ -5,7 +5,6 @@
 import { client, v1 } from "@datadog/datadog-api-client";
 
 const configuration = client.createConfiguration();
-configuration.unstableOperations["v1.getSpecifiedMonthlyCustomReports"] = true;
 const apiInstance = new v1.UsageMeteringApi(configuration);
 
 const params: v1.UsageMeteringApiGetSpecifiedMonthlyCustomReportsRequest = {
