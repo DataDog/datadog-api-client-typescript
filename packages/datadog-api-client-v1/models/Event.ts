ca6eaa135c2d2c4b79cc7bc81f8ec9aaa4ec17f2
ci.datadog-api-spec <packages@datadoghq.com>
2022-08-23 23:18:07 +0000

    Regenerate client from commit ccfa4a07 of spec repo

--- packages/datadog-api-client-v1/models/Event.ts
+++ packages/datadog-api-client-v1/models/Event.ts
@@ -51,7 +51,7 @@ export class Event {
   "priority"?: EventPriority;
   /**
    * The type of event being posted. Option examples include nagios, hudson, jenkins, my_apps, chef, puppet, git, bitbucket, etc.
-   * The list of standard source attribute values [available here](https://docs.datadoghq.com/integrations/faq/list-of-api-source-attribute-value).
+   * The list of standard source attribute values [available here](https://docs.datadoghq.com/events/guides/new_events_sources/).
    */
   "sourceTypeName"?: string;
   /**
