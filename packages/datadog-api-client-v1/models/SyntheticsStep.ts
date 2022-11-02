c080b8d70712f11e50d9b75281e1ede573500baa
ci.datadog-api-spec <packages@datadoghq.com>
2022-11-02 20:12:26 +0000

    Regenerate client from commit 7c5579df of spec repo

--- packages/datadog-api-client-v1/models/SyntheticsStep.ts
+++ packages/datadog-api-client-v1/models/SyntheticsStep.ts
@@ -24,6 +24,10 @@ export class SyntheticsStep {
    */
   "name"?: string;
   /**
+   * A boolean set to not take a screenshot for the step.
+   */
+  "noScreenshot"?: boolean;
+  /**
    * The parameters of the step.
    */
   "params"?: any;
@@ -56,6 +60,10 @@ export class SyntheticsStep {
     name: {
       baseName: "name",
       type: "string",
+    },
+    noScreenshot: {
+      baseName: "noScreenshot",
+      type: "boolean",
     },
     params: {
       baseName: "params",
