6ebc5ce2fe2da4893e7fa6215a8343e07f0d24ac
Sherzod Karimov <sherzod.karimov@datadoghq.com>
2022-11-07 10:26:49 -0500

    lint

--- userAgent.ts
+++ userAgent.ts
@@ -3,7 +3,7 @@ import { version } from "./version";
 export let userAgent: string;
 if (typeof process !== 'undefined' && process.release.name === 'node') {
     const os = require("os");
-    userAgent = `datadog-api-client-typescriptaaa/${version} (node ${process.versions.node}; os ${os.type()}; arch ${os.arch()})`
+    userAgent = `datadog-api-client-typescript/${version} (node ${process.versions.node}; os ${os.type()}; arch ${os.arch()})`
 } else if (typeof window !== "undefined" && typeof window.document !== "undefined") {
     // we don't set user-agent headers in browsers
 } else {
