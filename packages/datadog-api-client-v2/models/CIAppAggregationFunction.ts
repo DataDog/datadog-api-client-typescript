443a8b48c370ab305069014e22416185907492dd
ci.datadog-api-spec <packages@datadoghq.com>
2022-11-17 17:20:39 +0000

    Regenerate client from commit d582c49d of spec repo

--- packages/datadog-api-client-v2/models/CIAppAggregationFunction.ts
+++ packages/datadog-api-client-v2/models/CIAppAggregationFunction.ts
@@ -16,7 +16,11 @@ export type CIAppAggregationFunction =
   | typeof MIN
   | typeof MAX
   | typeof AVG
-  | typeof MEDIAN;
+  | typeof MEDIAN
+  | typeof LATEST
+  | typeof EARLIEST
+  | typeof MOST_FREQUENT
+  | typeof DELTA;
 export const COUNT = "count";
 export const CARDINALITY = "cardinality";
 export const PERCENTILE_75 = "pc75";
@@ -29,3 +33,7 @@ export const MIN = "min";
 export const MAX = "max";
 export const AVG = "avg";
 export const MEDIAN = "median";
+export const LATEST = "latest";
+export const EARLIEST = "earliest";
+export const MOST_FREQUENT = "most_frequent";
+export const DELTA = "delta";
