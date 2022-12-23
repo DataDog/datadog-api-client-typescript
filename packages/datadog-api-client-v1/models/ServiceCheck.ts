3fa0f040725a4e849eace9239397bb022dac320d
ci.datadog-api-spec <packages@datadoghq.com>
2022-12-23 14:10:42 +0000

    Regenerate client from commit c3b2f174 of spec repo

--- packages/datadog-api-client-v1/models/ServiceCheck.ts
+++ packages/datadog-api-client-v1/models/ServiceCheck.ts
@@ -24,7 +24,7 @@ export class ServiceCheck {
    */
   "message"?: string;
   /**
-   * The status of a service check.
+   * The status of a service check. Set to `0` for OK, `1` for warning, `2` for critical, and `3` for unknown.
    */
   "status": ServiceCheckStatus;
   /**
