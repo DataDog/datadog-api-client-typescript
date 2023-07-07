e5f16644f7912da914256a27ca686731c493ce36
ci.datadog-api-spec <packages@datadoghq.com>
2023-07-07 16:56:38 +0000

    Regenerate client from commit 64593ced of spec repo

--- packages/datadog-api-client-v2/models/ConfluentResourceResponseAttributes.ts
+++ packages/datadog-api-client-v2/models/ConfluentResourceResponseAttributes.ts
@@ -15,6 +15,10 @@ export class ConfluentResourceResponseAttributes {
    */
   "enableCustomMetrics"?: boolean;
   /**
+   * The ID associated with the Confluent resource.
+   */
+  "id"?: string;
+  /**
    * The resource type of the Resource. Can be `kafka`, `connector`, `ksql`, or `schema_registry`.
    */
   "resourceType": string;
@@ -35,6 +39,10 @@ export class ConfluentResourceResponseAttributes {
     enableCustomMetrics: {
       baseName: "enable_custom_metrics",
       type: "boolean",
+    },
+    id: {
+      baseName: "id",
+      type: "string",
     },
     resourceType: {
       baseName: "resource_type",
