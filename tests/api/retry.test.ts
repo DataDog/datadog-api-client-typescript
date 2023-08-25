11fc11faa4cdfcfbfdb0fab535c7de6c6fcce225
HantingZhang2 <hanting.zhang@datadoghq.com>
2023-08-25 15:19:06 +0200

    Test sleep time

--- tests/api/retry.test.ts
+++ tests/api/retry.test.ts
@@ -11,29 +11,27 @@ describe("IsomorphicFetchHttpLibrary Retry Test", () => {
     const fakeRequestContext = new RequestContext("https://retry.test.com",HttpMethod.GET);
 
   beforeAll(() => {
-    nock.disableNetConnect(); // Prevent actual network requests
+    nock.disableNetConnect();
   });
 
   afterAll(() => {
     nock.cleanAll();
-    nock.enableNetConnect(); // Re-enable network connections
+    nock.enableNetConnect();
   });
 
   it("should retry based on response code and retry settings", async () => {
-    // Set up your mock responses
     nock("https://retry.test.com")
       .get("/")
-      .reply(429, {}, { 'x-ratelimit-reset': '1' })
+      .reply(429, 'Too Many Requests', { 'x-ratelimit-reset' : '1' })
 
       .get("/")
-      .reply(429, {}, { 'x-ratelimit-reset': '1' }) 
+      .reply(429, 'Too Many Requests', { 'x-ratelimit-reset': '1' }) 
 
       .get("/")
-      .reply(429, {}, { 'x-ratelimit-reset': '1' })
+      .reply(429, 'Too Many Requests', { 'x-ratelimit-reset': '1' })
 
       .get("/")
-      .reply(200, { data: "success" }); // Simulate successful response
-
+      .reply(200, 'OK'); 
     const httpLibrary = new IsomorphicFetchHttpLibrary();
     httpLibrary['sleep'] = jest.fn(() => Promise.resolve());
 
@@ -45,7 +43,10 @@ describe("IsomorphicFetchHttpLibrary Retry Test", () => {
     const response = await httpLibrary.send(fakeRequestContext);
 
     expect(response.httpStatusCode).toBe(200);
-
+    expect(httpLibrary['sleep']).toHaveBeenCalledTimes(3);
+    expect(httpLibrary['sleep']).toHaveBeenCalledWith(2000);
+    expect(httpLibrary['sleep']).toHaveBeenCalledWith(4000);
+    expect(httpLibrary['sleep']).toHaveBeenCalledWith(8000);
     expect(nock.isDone()).toBe(true); // Ensure all expected requests were made
   });
 });
