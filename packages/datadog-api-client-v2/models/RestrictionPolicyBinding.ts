86353b3a1b72459bf64a40a4de041029a335e9fe
ci.datadog-api-spec <packages@datadoghq.com>
2023-10-12 18:35:13 +0000

    Regenerate client from commit 1ddbcdce of spec repo

--- packages/datadog-api-client-v2/models/RestrictionPolicyBinding.ts
+++ packages/datadog-api-client-v2/models/RestrictionPolicyBinding.ts
@@ -12,7 +12,7 @@ import { AttributeTypeMap } from "../../datadog-api-client-common/util";
 export class RestrictionPolicyBinding {
   /**
    * An array of principals. A principal is a subject or group of subjects.
-   * Each principal is formatted as `type:id`. Supported types: `role`, `team` (beta), `user` (beta), and `org`.
+   * Each principal is formatted as `type:id`. Supported types: `role`, `team`, `user`, and `org`.
    * The org ID can be obtained through the api/v2/current_user API.
    * The user principal type accepts service account IDs.
    */
