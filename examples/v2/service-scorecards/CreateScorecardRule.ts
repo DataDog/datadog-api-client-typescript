c7986bc3c4f65776971391e001c02c6f038a5842
ci.datadog-api-spec <packages@datadoghq.com>
2023-11-06 14:31:21 +0000

    Regenerate client from commit c1794418 of spec repo

--- examples/v2/service-scorecards/CreateScorecardRule.ts
+++ examples/v2/service-scorecards/CreateScorecardRule.ts
@@ -5,8 +5,8 @@
 import { client, v2 } from "@datadog/datadog-api-client";
 
 const configuration = client.createConfiguration();
-configuration.unstableOperations["v2.createScorecardRule"] = true;
 configuration.unstableOperations["v2.createScorecardOutcomesBatch"] = true;
+configuration.unstableOperations["v2.createScorecardRule"] = true;
 const apiInstance = new v2.ServiceScorecardsApi(configuration);
 
 const params: v2.ServiceScorecardsApiCreateScorecardRuleRequest = {
