0490e90957c83a17d99b32c127107aee5ecdb63f
ci.datadog-api-spec <packages@datadoghq.com>
2023-11-09 22:08:57 +0000

    Regenerate client from commit 893a406f of spec repo

--- packages/datadog-api-client-v2/apis/MetricsApi.ts
+++ packages/datadog-api-client-v2/apis/MetricsApi.ts
@@ -2117,7 +2117,8 @@ export class MetricsApi {
 
   /**
    * Update the tag configuration of a metric or percentile aggregations of a distribution metric or custom aggregations
-   * of a count, rate, or gauge metric.
+   * of a count, rate, or gauge metric. By setting `exclude_tags_mode` to true the behavior is changed
+   * from an allow-list to a deny-list, and tags in the defined list will not be queryable.
    * Can only be used with application keys from users with the `Manage Tags for Metrics` permission.
    * @param param The request object
    */
