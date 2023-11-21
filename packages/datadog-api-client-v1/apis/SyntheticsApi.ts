bc269924b032bd26f77fc0000360cea5fd0c9fd4
ci.datadog-api-spec <packages@datadoghq.com>
2023-11-21 21:49:41 +0000

    Regenerate client from commit 891a7c39 of spec repo

--- packages/datadog-api-client-v1/apis/SyntheticsApi.ts
+++ packages/datadog-api-client-v1/apis/SyntheticsApi.ts
@@ -928,6 +928,7 @@ export class SyntheticsApiRequestFactory extends BaseAPIRequestFactory {
 
     // Apply auth methods
     applySecurityAuthentication(_config, requestContext, [
+      "AuthZ",
       "apiKeyAuth",
       "appKeyAuth",
     ]);
