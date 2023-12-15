1da7e3a2f77062bf957e034c9736f07c811b35f0
ci.datadog-api-spec <packages@datadoghq.com>
2023-12-15 15:52:13 +0000

    Regenerate client from commit 56bcf717 of spec repo

--- examples/v2/gcp-integration/CreateGCPSTSAccount_4235664992.ts
+++ examples/v2/gcp-integration/CreateGCPSTSAccount_4235664992.ts
@@ -12,6 +12,7 @@ const params: v2.GCPIntegrationApiCreateGCPSTSAccountRequest = {
     data: {
       attributes: {
         isCspmEnabled: true,
+        resourceCollectionEnabled: true,
         clientEmail: "252bf553ef04b351@test-project.iam.gserviceaccount.com",
         hostFilters: [],
       },
