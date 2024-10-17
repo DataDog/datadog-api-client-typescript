e293683b26d021165ba2a5a992e5de0b6f06eaa2
ci.datadog-api-spec <packages@datadoghq.com>
2024-10-17 16:53:41 +0000

    Regenerate client from commit 3cbe2c1b of spec repo

---
+++ packages/datadog-api-client-v2/models/ChangeEventCreateRequest.ts
@@ -0,0 +1,62 @@
+/**
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2020-Present Datadog, Inc.
+ */
+import { ChangeEvent } from "./ChangeEvent";
+import { ChangeEventCreateRequestType } from "./ChangeEventCreateRequestType";
+
+import { AttributeTypeMap } from "../../datadog-api-client-common/util";
+
+/**
+ * Object representing an event.
+ */
+export class ChangeEventCreateRequest {
+  /**
+   * Event attributes.
+   */
+  "attributes"?: ChangeEvent;
+  /**
+   * Entity type.
+   */
+  "type"?: ChangeEventCreateRequestType;
+
+  /**
+   * A container for additional, undeclared properties.
+   * This is a holder for any undeclared properties as specified with
+   * the 'additionalProperties' keyword in the OAS document.
+   */
+  "additionalProperties"?: { [key: string]: any };
+
+  /**
+   * @ignore
+   */
+  "_unparsed"?: boolean;
+
+  /**
+   * @ignore
+   */
+  static readonly attributeTypeMap: AttributeTypeMap = {
+    attributes: {
+      baseName: "attributes",
+      type: "ChangeEvent",
+    },
+    type: {
+      baseName: "type",
+      type: "ChangeEventCreateRequestType",
+    },
+    additionalProperties: {
+      baseName: "additionalProperties",
+      type: "any",
+    },
+  };
+
+  /**
+   * @ignore
+   */
+  static getAttributeTypeMap(): AttributeTypeMap {
+    return ChangeEventCreateRequest.attributeTypeMap;
+  }
+
+  public constructor() {}
+}
