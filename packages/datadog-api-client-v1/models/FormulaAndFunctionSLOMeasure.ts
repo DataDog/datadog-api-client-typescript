5e2746379ad03bfc1e4f54aec89dd380562ced0c
ci.datadog-api-spec <packages@datadoghq.com>
2024-11-11 17:26:46 +0000

    Regenerate client from commit 9de1804a of spec repo

--- packages/datadog-api-client-v1/models/FormulaAndFunctionSLOMeasure.ts
+++ packages/datadog-api-client-v1/models/FormulaAndFunctionSLOMeasure.ts
@@ -13,6 +13,8 @@ import { UnparsedObject } from "../../datadog-api-client-common/util";
 export type FormulaAndFunctionSLOMeasure =
   | typeof GOOD_EVENTS
   | typeof BAD_EVENTS
+  | typeof GOOD_MINUTES
+  | typeof BAD_MINUTES
   | typeof SLO_STATUS
   | typeof ERROR_BUDGET_REMAINING
   | typeof BURN_RATE
@@ -20,6 +22,8 @@ export type FormulaAndFunctionSLOMeasure =
   | UnparsedObject;
 export const GOOD_EVENTS = "good_events";
 export const BAD_EVENTS = "bad_events";
+export const GOOD_MINUTES = "good_minutes";
+export const BAD_MINUTES = "bad_minutes";
 export const SLO_STATUS = "slo_status";
 export const ERROR_BUDGET_REMAINING = "error_budget_remaining";
 export const BURN_RATE = "burn_rate";
