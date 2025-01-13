1e1adc66e0eaf5a6a2c64f599fce74dc51c90ea1
skarimo <40482491+skarimo@users.noreply.github.com>
2025-01-13 12:43:36 -0500

    Refactor fetch handling to avoid illegal invocation errors (#1990)
    * refactor fetch handling
    
    * avoid && taking precedence
    
    * add a test

--- tests/api/server.test.ts
+++ tests/api/server.test.ts
@@ -3,7 +3,8 @@ import { createConfiguration } from "../../packages/datadog-api-client-common/co
 import {
   HttpMethod,
 } from "../../packages/datadog-api-client-common/http/http";
-import { DashboardsApiRequestFactory } from '../../packages/datadog-api-client-v1/apis/DashboardsApi';
+import { DashboardsApiRequestFactory, DashboardsApi } from '../../packages/datadog-api-client-v1/apis/DashboardsApi';
+import { logger } from "../../logger";
 
 test('TestServerClone', () => {
     const newServer = server1.clone();
@@ -52,5 +53,18 @@ test ('TestBackoffBaseNoLessThanTwo',() => {
       backoffBase : 1,
     }; 
    expect(()=> createConfiguration(configOpts)).toThrow();
+  }
+);
+
+test('TestCustomFetch', async () => {
+    // Mock logger to prevent writing to console during test
+    logger.error = jest.fn();
+    // Mock fetch
+    const _fetch = jest.fn(() => Promise.resolve());
+    const config = createConfiguration({fetch: _fetch});
+    const api = new DashboardsApi(config);
+    await expect(api.deleteDashboard({dashboardId: "id"})).rejects.toThrow();
+
+    expect(_fetch).toHaveBeenCalledTimes(1);
   }
 );
