e5c0534d4905f0ac5516e91c031e6dc82dc09813
ci.datadog-api-spec <packages@datadoghq.com>
2025-02-06 18:01:05 +0000

    Regenerate client from commit b89b292b of spec repo

--- packages/datadog-api-client-v2/apis/MetricsApi.ts
+++ packages/datadog-api-client-v2/apis/MetricsApi.ts
@@ -403,6 +403,7 @@ export class MetricsApiRequestFactory extends BaseAPIRequestFactory {
     filterIncludePercentiles?: boolean,
     filterQueried?: boolean,
     filterTags?: string,
+    filterRelatedAssets?: boolean,
     windowSeconds?: number,
     pageSize?: number,
     pageCursor?: string,
@@ -464,6 +465,13 @@ export class MetricsApiRequestFactory extends BaseAPIRequestFactory {
       requestContext.setQueryParam(
         "filter[tags]",
         ObjectSerializer.serialize(filterTags, "string", ""),
+        ""
+      );
+    }
+    if (filterRelatedAssets !== undefined) {
+      requestContext.setQueryParam(
+        "filter[related_assets]",
+        ObjectSerializer.serialize(filterRelatedAssets, "boolean", ""),
         ""
       );
     }
@@ -1817,7 +1825,7 @@ export interface MetricsApiListTagConfigurationsRequest {
    */
   filterIncludePercentiles?: boolean;
   /**
-   * (Beta) Filter custom metrics that have or have not been queried in the specified window[seconds].
+   * (Preview) Filter custom metrics that have or have not been queried in the specified window[seconds].
    * If no window is provided or the window is less than 2 hours, a default of 2 hours will be applied.
    * @type boolean
    */
@@ -1828,6 +1836,11 @@ export interface MetricsApiListTagConfigurationsRequest {
    * @type string
    */
   filterTags?: string;
+  /**
+   * (Preview) Filter metrics that are used in dashboards, monitors, notebooks, SLOs.
+   * @type boolean
+   */
+  filterRelatedAssets?: boolean;
   /**
    * The number of seconds of look back (from now) to apply to a filter[tag] or filter[queried] query.
    * Default value is 3600 (1 hour), maximum value is 2,592,000 (30 days).
@@ -2138,6 +2151,7 @@ export class MetricsApi {
       param.filterIncludePercentiles,
       param.filterQueried,
       param.filterTags,
+      param.filterRelatedAssets,
       param.windowSeconds,
       param.pageSize,
       param.pageCursor,
@@ -2172,6 +2186,7 @@ export class MetricsApi {
         param.filterIncludePercentiles,
         param.filterQueried,
         param.filterTags,
+        param.filterRelatedAssets,
         param.windowSeconds,
         param.pageSize,
         param.pageCursor,
