0da9f06f492cc40612033d221f50d8bfcfe48622
ci.datadog-api-spec <packages@datadoghq.com>
2025-03-28 19:13:57 +0000

    Regenerate client from commit 51bfdf43 of spec repo

--- packages/datadog-api-client-v2/models/SecurityMonitoringStandardDataSource.ts
+++ packages/datadog-api-client-v2/models/SecurityMonitoringStandardDataSource.ts
@@ -13,6 +13,14 @@ import { UnparsedObject } from "../../datadog-api-client-common/util";
 export type SecurityMonitoringStandardDataSource =
   | typeof LOGS
   | typeof AUDIT
+  | typeof APP_SEC_SPANS
+  | typeof SPANS
+  | typeof SECURITY_RUNTIME
+  | typeof NETWORK
   | UnparsedObject;
 export const LOGS = "logs";
 export const AUDIT = "audit";
+export const APP_SEC_SPANS = "app_sec_spans";
+export const SPANS = "spans";
+export const SECURITY_RUNTIME = "security_runtime";
+export const NETWORK = "network";
