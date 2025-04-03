9276e78a95f3ecfdd7d2a4eb32ccb9894095b159
Sherzod Karimov <sherzod.karimov@datadoghq.com>
2025-04-03 13:15:06 -0400

    add basic linter and formatter

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    // // TODO: Enable and fix all issues after the migration and tests are passing
+    // tseslint.configs.recommended,
+    // tseslint.configs.strict,
+    // tseslint.configs.stylistic,
+);
