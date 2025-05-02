5f38f03b7bac452518ec13adbe3e93c41def7cb7
ci.datadog-api-spec <packages@datadoghq.com>
2025-05-02 14:23:07 +0000

    Regenerate client from commit 022c8d0f of spec repo

---
+++ packages/datadog-api-client-v1/models/SyntheticsAssertionTargetValue.ts
@@ -0,0 +1,13 @@
+/**
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2020-Present Datadog, Inc.
+ */
+
+import { UnparsedObject } from "../../datadog-api-client-common/util";
+
+/**
+ * Value used by the operator in assertions. Can be either a number or string.
+ */
+
+export type SyntheticsAssertionTargetValue = number | string | UnparsedObject;
