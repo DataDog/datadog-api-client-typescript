be74f3c34c83f5b3523fad9e14813caf904c49c1
ci.datadog-api-spec <packages@datadoghq.com>
2025-05-12 09:42:04 +0000

    Regenerate client from commit c4d0aeef of spec repo

---
+++ examples/v2/on-call-paging/ResolveOnCallPage.ts
@@ -0,0 +1,21 @@
+/**
+ * Resolve On-Call Page returns "Accepted." response
+ */
+
+import { client, v2 } from "@datadog/datadog-api-client";
+
+const configuration = client.createConfiguration();
+const apiInstance = new v2.OnCallPagingApi(configuration);
+
+const params: v2.OnCallPagingApiResolveOnCallPageRequest = {
+  pageId: "15e74b8b-f865-48d0-bcc5-453323ed2c8f",
+};
+
+apiInstance
+  .resolveOnCallPage(params)
+  .then((data: any) => {
+    console.log(
+      "API called successfully. Returned data: " + JSON.stringify(data)
+    );
+  })
+  .catch((error: any) => console.error(error));
---
+++ packages/datadog-api-client-v2/models/CreatePageRequest.ts
@@ -0,0 +1,53 @@
+/**
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2020-Present Datadog, Inc.
+ */
+import { CreatePageRequestData } from "./CreatePageRequestData";
+
+import { AttributeTypeMap } from "../../datadog-api-client-common/util";
+
+/**
+ * Full request to trigger an On-Call Page.
+ */
+export class CreatePageRequest {
+  /**
+   * The main request body, including attributes and resource type.
+   */
+  "data"?: CreatePageRequestData;
+
+  /**
+   * A container for additional, undeclared properties.
+   * This is a holder for any undeclared properties as specified with
+   * the 'additionalProperties' keyword in the OAS document.
+   */
+  "additionalProperties"?: { [key: string]: any };
+
+  /**
+   * @ignore
+   */
+  "_unparsed"?: boolean;
+
+  /**
+   * @ignore
+   */
+  static readonly attributeTypeMap: AttributeTypeMap = {
+    data: {
+      baseName: "data",
+      type: "CreatePageRequestData",
+    },
+    additionalProperties: {
+      baseName: "additionalProperties",
+      type: "any",
+    },
+  };
+
+  /**
+   * @ignore
+   */
+  static getAttributeTypeMap(): AttributeTypeMap {
+    return CreatePageRequest.attributeTypeMap;
+  }
+
+  public constructor() {}
+}
