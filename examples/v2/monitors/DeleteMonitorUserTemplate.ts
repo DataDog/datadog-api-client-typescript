fd455ac03188b25ac8f1f01998d3af9defe2d07b
ci.datadog-api-spec <packages@datadoghq.com>
2025-05-24 03:08:23 +0000

    Regenerate client from commit 39351bc5 of spec repo

---
+++ examples/v2/monitors/DeleteMonitorUserTemplate.ts
@@ -0,0 +1,25 @@
+/**
+ * Delete a monitor user template returns "OK" response
+ */
+
+import { client, v2 } from "@datadog/datadog-api-client";
+
+const configuration = client.createConfiguration();
+const apiInstance = new v2.MonitorsApi(configuration);
+
+// there is a valid "monitor_user_template" in the system
+const MONITOR_USER_TEMPLATE_DATA_ID = process.env
+  .MONITOR_USER_TEMPLATE_DATA_ID as string;
+
+const params: v2.MonitorsApiDeleteMonitorUserTemplateRequest = {
+  templateId: MONITOR_USER_TEMPLATE_DATA_ID,
+};
+
+apiInstance
+  .deleteMonitorUserTemplate(params)
+  .then((data: any) => {
+    console.log(
+      "API called successfully. Returned data: " + JSON.stringify(data)
+    );
+  })
+  .catch((error: any) => console.error(error));
---
+++ packages/datadog-api-client-v2/models/MonitorUserTemplateResponseDataWithVersions.ts
@@ -0,0 +1,70 @@
+/**
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2020-Present Datadog, Inc.
+ */
+import { MonitorUserTemplate } from "./MonitorUserTemplate";
+import { MonitorUserTemplateResourceType } from "./MonitorUserTemplateResourceType";
+
+import { AttributeTypeMap } from "../../datadog-api-client-common/util";
+
+/**
+ * Monitor user template data.
+ */
+export class MonitorUserTemplateResponseDataWithVersions {
+  /**
+   * A monitor user template object.
+   */
+  "attributes"?: MonitorUserTemplate;
+  /**
+   * The unique identifier.
+   */
+  "id"?: string;
+  /**
+   * Monitor user template resource type.
+   */
+  "type"?: MonitorUserTemplateResourceType;
+
+  /**
+   * A container for additional, undeclared properties.
+   * This is a holder for any undeclared properties as specified with
+   * the 'additionalProperties' keyword in the OAS document.
+   */
+  "additionalProperties"?: { [key: string]: any };
+
+  /**
+   * @ignore
+   */
+  "_unparsed"?: boolean;
+
+  /**
+   * @ignore
+   */
+  static readonly attributeTypeMap: AttributeTypeMap = {
+    attributes: {
+      baseName: "attributes",
+      type: "MonitorUserTemplate",
+    },
+    id: {
+      baseName: "id",
+      type: "string",
+    },
+    type: {
+      baseName: "type",
+      type: "MonitorUserTemplateResourceType",
+    },
+    additionalProperties: {
+      baseName: "additionalProperties",
+      type: "any",
+    },
+  };
+
+  /**
+   * @ignore
+   */
+  static getAttributeTypeMap(): AttributeTypeMap {
+    return MonitorUserTemplateResponseDataWithVersions.attributeTypeMap;
+  }
+
+  public constructor() {}
+}
