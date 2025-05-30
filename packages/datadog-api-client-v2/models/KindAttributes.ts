826a0631c2ec9e0988cb61a39538593351b3baf6
ci.datadog-api-spec <packages@datadoghq.com>
2025-05-30 12:40:41 +0000

    Regenerate client from commit e4b2672f of spec repo

---
+++ packages/datadog-api-client-v2/models/KindAttributes.ts
@@ -0,0 +1,68 @@
+/**
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2020-Present Datadog, Inc.
+ */
+
+import { AttributeTypeMap } from "../../datadog-api-client-common/util";
+
+/**
+ * Kind attributes.
+ */
+export class KindAttributes {
+  /**
+   * Short description of the kind.
+   */
+  "description"?: string;
+  /**
+   * User friendly name of the kind.
+   */
+  "displayName"?: string;
+  /**
+   * The kind name.
+   */
+  "name"?: string;
+
+  /**
+   * A container for additional, undeclared properties.
+   * This is a holder for any undeclared properties as specified with
+   * the 'additionalProperties' keyword in the OAS document.
+   */
+  "additionalProperties"?: { [key: string]: any };
+
+  /**
+   * @ignore
+   */
+  "_unparsed"?: boolean;
+
+  /**
+   * @ignore
+   */
+  static readonly attributeTypeMap: AttributeTypeMap = {
+    description: {
+      baseName: "description",
+      type: "string",
+    },
+    displayName: {
+      baseName: "displayName",
+      type: "string",
+    },
+    name: {
+      baseName: "name",
+      type: "string",
+    },
+    additionalProperties: {
+      baseName: "additionalProperties",
+      type: "any",
+    },
+  };
+
+  /**
+   * @ignore
+   */
+  static getAttributeTypeMap(): AttributeTypeMap {
+    return KindAttributes.attributeTypeMap;
+  }
+
+  public constructor() {}
+}
