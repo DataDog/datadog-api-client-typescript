84a50a8626d1d06bcc5321595eb3b7328bb901db
ci.datadog-api-spec <packages@datadoghq.com>
2025-06-10 18:47:30 +0000

    Regenerate client from commit 87839d6 of spec repo

--- services/software_catalog/src/v2/SoftwareCatalogApi.ts
+++ services/software_catalog/src/v2/SoftwareCatalogApi.ts
@@ -25,13 +25,17 @@ import { TypingInfo } from "./models/TypingInfo";
 import { APIErrorResponse } from "./models/APIErrorResponse";
 import { EntityData } from "./models/EntityData";
 import { IncludeType } from "./models/IncludeType";
+import { KindData } from "./models/KindData";
 import { ListEntityCatalogResponse } from "./models/ListEntityCatalogResponse";
+import { ListKindCatalogResponse } from "./models/ListKindCatalogResponse";
 import { ListRelationCatalogResponse } from "./models/ListRelationCatalogResponse";
 import { RelationIncludeType } from "./models/RelationIncludeType";
 import { RelationResponse } from "./models/RelationResponse";
 import { RelationType } from "./models/RelationType";
 import { UpsertCatalogEntityRequest } from "./models/UpsertCatalogEntityRequest";
 import { UpsertCatalogEntityResponse } from "./models/UpsertCatalogEntityResponse";
+import { UpsertCatalogKindRequest } from "./models/UpsertCatalogKindRequest";
+import { UpsertCatalogKindResponse } from "./models/UpsertCatalogKindResponse";
 import { version } from "../version";
 
 export class SoftwareCatalogApiRequestFactory extends BaseAPIRequestFactory {
@@ -63,6 +67,51 @@ export class SoftwareCatalogApiRequestFactory extends BaseAPIRequestFactory {
     // Make Request Context
     const { server, overrides } = _config.getServerAndOverrides(
       "SoftwareCatalogApi.v2.deleteCatalogEntity",
+      SoftwareCatalogApi.operationServers,
+    );
+    const requestContext = server.makeRequestContext(
+      localVarPath,
+      HttpMethod.DELETE,
+      overrides,
+    );
+    requestContext.setHeaderParam("Accept", "*/*");
+    requestContext.setHttpConfig(_config.httpConfig);
+
+    // Set User-Agent
+    if (this.userAgent) {
+      requestContext.setHeaderParam("User-Agent", this.userAgent);
+    }
+
+    // Apply auth methods
+    applySecurityAuthentication(_config, requestContext, [
+      "AuthZ",
+      "apiKeyAuth",
+      "appKeyAuth",
+    ]);
+
+    return requestContext;
+  }
+
+  public async deleteCatalogKind(
+    kindId: string,
+    _options?: Configuration,
+  ): Promise<RequestContext> {
+    const _config = _options || this.configuration;
+
+    // verify required parameter 'kindId' is not null or undefined
+    if (kindId === null || kindId === undefined) {
+      throw new RequiredError("kindId", "deleteCatalogKind");
+    }
+
+    // Path Params
+    const localVarPath = "/api/v2/catalog/kind/{kind_id}".replace(
+      "{kind_id}",
+      encodeURIComponent(String(kindId)),
+    );
+
+    // Make Request Context
+    const { server, overrides } = _config.getServerAndOverrides(
+      "SoftwareCatalogApi.v2.deleteCatalogKind",
       SoftwareCatalogApi.operationServers,
     );
     const requestContext = server.makeRequestContext(
@@ -192,6 +241,76 @@ export class SoftwareCatalogApiRequestFactory extends BaseAPIRequestFactory {
       requestContext.setQueryParam(
         "include",
         serialize(include, TypingInfo, "IncludeType", ""),
+        "",
+      );
+    }
+
+    // Apply auth methods
+    applySecurityAuthentication(_config, requestContext, [
+      "AuthZ",
+      "apiKeyAuth",
+      "appKeyAuth",
+    ]);
+
+    return requestContext;
+  }
+
+  public async listCatalogKind(
+    pageOffset?: number,
+    pageLimit?: number,
+    filterId?: string,
+    filterName?: string,
+    _options?: Configuration,
+  ): Promise<RequestContext> {
+    const _config = _options || this.configuration;
+
+    // Path Params
+    const localVarPath = "/api/v2/catalog/kind";
+
+    // Make Request Context
+    const { server, overrides } = _config.getServerAndOverrides(
+      "SoftwareCatalogApi.v2.listCatalogKind",
+      SoftwareCatalogApi.operationServers,
+    );
+    const requestContext = server.makeRequestContext(
+      localVarPath,
+      HttpMethod.GET,
+      overrides,
+    );
+    requestContext.setHeaderParam("Accept", "application/json");
+    requestContext.setHttpConfig(_config.httpConfig);
+
+    // Set User-Agent
+    if (this.userAgent) {
+      requestContext.setHeaderParam("User-Agent", this.userAgent);
+    }
+
+    // Query Params
+    if (pageOffset !== undefined) {
+      requestContext.setQueryParam(
+        "page[offset]",
+        serialize(pageOffset, TypingInfo, "number", "int64"),
+        "",
+      );
+    }
+    if (pageLimit !== undefined) {
+      requestContext.setQueryParam(
+        "page[limit]",
+        serialize(pageLimit, TypingInfo, "number", "int64"),
+        "",
+      );
+    }
+    if (filterId !== undefined) {
+      requestContext.setQueryParam(
+        "filter[id]",
+        serialize(filterId, TypingInfo, "string", ""),
+        "",
+      );
+    }
+    if (filterName !== undefined) {
+      requestContext.setQueryParam(
+        "filter[name]",
+        serialize(filterName, TypingInfo, "string", ""),
         "",
       );
     }
@@ -342,6 +461,57 @@ export class SoftwareCatalogApiRequestFactory extends BaseAPIRequestFactory {
 
     return requestContext;
   }
+
+  public async upsertCatalogKind(
+    body: UpsertCatalogKindRequest,
+    _options?: Configuration,
+  ): Promise<RequestContext> {
+    const _config = _options || this.configuration;
+
+    // verify required parameter 'body' is not null or undefined
+    if (body === null || body === undefined) {
+      throw new RequiredError("body", "upsertCatalogKind");
+    }
+
+    // Path Params
+    const localVarPath = "/api/v2/catalog/kind";
+
+    // Make Request Context
+    const { server, overrides } = _config.getServerAndOverrides(
+      "SoftwareCatalogApi.v2.upsertCatalogKind",
+      SoftwareCatalogApi.operationServers,
+    );
+    const requestContext = server.makeRequestContext(
+      localVarPath,
+      HttpMethod.POST,
+      overrides,
+    );
+    requestContext.setHeaderParam("Accept", "application/json");
+    requestContext.setHttpConfig(_config.httpConfig);
+
+    // Set User-Agent
+    if (this.userAgent) {
+      requestContext.setHeaderParam("User-Agent", this.userAgent);
+    }
+
+    // Body Params
+    const contentType = getPreferredMediaType(["application/json"]);
+    requestContext.setHeaderParam("Content-Type", contentType);
+    const serializedBody = stringify(
+      serialize(body, TypingInfo, "UpsertCatalogKindRequest", ""),
+      contentType,
+    );
+    requestContext.setBody(serializedBody);
+
+    // Apply auth methods
+    applySecurityAuthentication(_config, requestContext, [
+      "AuthZ",
+      "apiKeyAuth",
+      "appKeyAuth",
+    ]);
+
+    return requestContext;
+  }
 }
 
 export class SoftwareCatalogApiResponseProcessor {
@@ -353,6 +523,54 @@ export class SoftwareCatalogApiResponseProcessor {
    * @throws ApiException if the response code was not in [200, 299]
    */
   public async deleteCatalogEntity(response: ResponseContext): Promise<void> {
+    const contentType = normalizeMediaType(response.headers["content-type"]);
+    if (response.httpStatusCode === 204) {
+      return;
+    }
+    if (
+      response.httpStatusCode === 400 ||
+      response.httpStatusCode === 403 ||
+      response.httpStatusCode === 404 ||
+      response.httpStatusCode === 429
+    ) {
+      const bodyText = parse(await response.body.text(), contentType);
+      let body: APIErrorResponse;
+      try {
+        body = deserialize(
+          bodyText,
+          TypingInfo,
+          "APIErrorResponse",
+        ) as APIErrorResponse;
+      } catch (error) {
+        logger.debug(`Got error deserializing error: ${error}`);
+        throw new ApiException<APIErrorResponse>(
+          response.httpStatusCode,
+          bodyText,
+        );
+      }
+      throw new ApiException<APIErrorResponse>(response.httpStatusCode, body);
+    }
+
+    // Work around for missing responses in specification, e.g. for petstore.yaml
+    if (response.httpStatusCode >= 200 && response.httpStatusCode <= 299) {
+      return;
+    }
+
+    const body = (await response.body.text()) || "";
+    throw new ApiException<string>(
+      response.httpStatusCode,
+      'Unknown API Status Code!\nBody: "' + body + '"',
+    );
+  }
+
+  /**
+   * Unwraps the actual response sent by the server from the response context and deserializes the response content
+   * to the expected objects
+   *
+   * @params response Response returned by the server for a request to deleteCatalogKind
+   * @throws ApiException if the response code was not in [200, 299]
+   */
+  public async deleteCatalogKind(response: ResponseContext): Promise<void> {
     const contentType = normalizeMediaType(response.headers["content-type"]);
     if (response.httpStatusCode === 204) {
       return;
@@ -439,6 +657,66 @@ export class SoftwareCatalogApiResponseProcessor {
         "ListEntityCatalogResponse",
         "",
       ) as ListEntityCatalogResponse;
+      return body;
+    }
+
+    const body = (await response.body.text()) || "";
+    throw new ApiException<string>(
+      response.httpStatusCode,
+      'Unknown API Status Code!\nBody: "' + body + '"',
+    );
+  }
+
+  /**
+   * Unwraps the actual response sent by the server from the response context and deserializes the response content
+   * to the expected objects
+   *
+   * @params response Response returned by the server for a request to listCatalogKind
+   * @throws ApiException if the response code was not in [200, 299]
+   */
+  public async listCatalogKind(
+    response: ResponseContext,
+  ): Promise<ListKindCatalogResponse> {
+    const contentType = normalizeMediaType(response.headers["content-type"]);
+    if (response.httpStatusCode === 200) {
+      const body: ListKindCatalogResponse = deserialize(
+        parse(await response.body.text(), contentType),
+        TypingInfo,
+        "ListKindCatalogResponse",
+      ) as ListKindCatalogResponse;
+      return body;
+    }
+    if (
+      response.httpStatusCode === 400 ||
+      response.httpStatusCode === 403 ||
+      response.httpStatusCode === 429
+    ) {
+      const bodyText = parse(await response.body.text(), contentType);
+      let body: APIErrorResponse;
+      try {
+        body = deserialize(
+          bodyText,
+          TypingInfo,
+          "APIErrorResponse",
+        ) as APIErrorResponse;
+      } catch (error) {
+        logger.debug(`Got error deserializing error: ${error}`);
+        throw new ApiException<APIErrorResponse>(
+          response.httpStatusCode,
+          bodyText,
+        );
+      }
+      throw new ApiException<APIErrorResponse>(response.httpStatusCode, body);
+    }
+
+    // Work around for missing responses in specification, e.g. for petstore.yaml
+    if (response.httpStatusCode >= 200 && response.httpStatusCode <= 299) {
+      const body: ListKindCatalogResponse = deserialize(
+        parse(await response.body.text(), contentType),
+        TypingInfo,
+        "ListKindCatalogResponse",
+        "",
+      ) as ListKindCatalogResponse;
       return body;
     }
 
@@ -564,6 +842,66 @@ export class SoftwareCatalogApiResponseProcessor {
       'Unknown API Status Code!\nBody: "' + body + '"',
     );
   }
+
+  /**
+   * Unwraps the actual response sent by the server from the response context and deserializes the response content
+   * to the expected objects
+   *
+   * @params response Response returned by the server for a request to upsertCatalogKind
+   * @throws ApiException if the response code was not in [200, 299]
+   */
+  public async upsertCatalogKind(
+    response: ResponseContext,
+  ): Promise<UpsertCatalogKindResponse> {
+    const contentType = normalizeMediaType(response.headers["content-type"]);
+    if (response.httpStatusCode === 202) {
+      const body: UpsertCatalogKindResponse = deserialize(
+        parse(await response.body.text(), contentType),
+        TypingInfo,
+        "UpsertCatalogKindResponse",
+      ) as UpsertCatalogKindResponse;
+      return body;
+    }
+    if (
+      response.httpStatusCode === 400 ||
+      response.httpStatusCode === 403 ||
+      response.httpStatusCode === 429
+    ) {
+      const bodyText = parse(await response.body.text(), contentType);
+      let body: APIErrorResponse;
+      try {
+        body = deserialize(
+          bodyText,
+          TypingInfo,
+          "APIErrorResponse",
+        ) as APIErrorResponse;
+      } catch (error) {
+        logger.debug(`Got error deserializing error: ${error}`);
+        throw new ApiException<APIErrorResponse>(
+          response.httpStatusCode,
+          bodyText,
+        );
+      }
+      throw new ApiException<APIErrorResponse>(response.httpStatusCode, body);
+    }
+
+    // Work around for missing responses in specification, e.g. for petstore.yaml
+    if (response.httpStatusCode >= 200 && response.httpStatusCode <= 299) {
+      const body: UpsertCatalogKindResponse = deserialize(
+        parse(await response.body.text(), contentType),
+        TypingInfo,
+        "UpsertCatalogKindResponse",
+        "",
+      ) as UpsertCatalogKindResponse;
+      return body;
+    }
+
+    const body = (await response.body.text()) || "";
+    throw new ApiException<string>(
+      response.httpStatusCode,
+      'Unknown API Status Code!\nBody: "' + body + '"',
+    );
+  }
 }
 
 export interface SoftwareCatalogApiDeleteCatalogEntityRequest {
@@ -572,6 +910,14 @@ export interface SoftwareCatalogApiDeleteCatalogEntityRequest {
    * @type string
    */
   entityId: string;
+}
+
+export interface SoftwareCatalogApiDeleteCatalogKindRequest {
+  /**
+   * Entity kind.
+   * @type string
+   */
+  kindId: string;
 }
 
 export interface SoftwareCatalogApiListCatalogEntityRequest {
@@ -627,6 +973,29 @@ export interface SoftwareCatalogApiListCatalogEntityRequest {
   include?: IncludeType;
 }
 
+export interface SoftwareCatalogApiListCatalogKindRequest {
+  /**
+   * Specific offset to use as the beginning of the returned page.
+   * @type number
+   */
+  pageOffset?: number;
+  /**
+   * Maximum number of kinds in the response.
+   * @type number
+   */
+  pageLimit?: number;
+  /**
+   * Filter entities by UUID.
+   * @type string
+   */
+  filterId?: string;
+  /**
+   * Filter entities by name.
+   * @type string
+   */
+  filterName?: string;
+}
+
 export interface SoftwareCatalogApiListCatalogRelationRequest {
   /**
    * Specific offset to use as the beginning of the returned page.
@@ -668,6 +1037,14 @@ export interface SoftwareCatalogApiUpsertCatalogEntityRequest {
   body: UpsertCatalogEntityRequest;
 }
 
+export interface SoftwareCatalogApiUpsertCatalogKindRequest {
+  /**
+   * Kind YAML or JSON.
+   * @type UpsertCatalogKindRequest
+   */
+  body: UpsertCatalogKindRequest;
+}
+
 export class SoftwareCatalogApi {
   private requestFactory: SoftwareCatalogApiRequestFactory;
   private responseProcessor: SoftwareCatalogApiResponseProcessor;
@@ -705,6 +1082,27 @@ export class SoftwareCatalogApi {
         .send(requestContext)
         .then((responseContext) => {
           return this.responseProcessor.deleteCatalogEntity(responseContext);
+        });
+    });
+  }
+
+  /**
+   * Delete a single kind in Software Catalog.
+   * @param param The request object
+   */
+  public deleteCatalogKind(
+    param: SoftwareCatalogApiDeleteCatalogKindRequest,
+    options?: Configuration,
+  ): Promise<void> {
+    const requestContextPromise = this.requestFactory.deleteCatalogKind(
+      param.kindId,
+      options,
+    );
+    return requestContextPromise.then((requestContext) => {
+      return this.configuration.httpApi
+        .send(requestContext)
+        .then((responseContext) => {
+          return this.responseProcessor.deleteCatalogKind(responseContext);
         });
     });
   }
@@ -770,6 +1168,74 @@ export class SoftwareCatalogApi {
 
       const response =
         await this.responseProcessor.listCatalogEntity(responseContext);
+      const responseData = response.data;
+      if (responseData === undefined) {
+        break;
+      }
+      const results = responseData;
+      for (const item of results) {
+        yield item;
+      }
+      if (results.length < pageSize) {
+        break;
+      }
+      if (param.pageOffset === undefined) {
+        param.pageOffset = pageSize;
+      } else {
+        param.pageOffset = param.pageOffset + pageSize;
+      }
+    }
+  }
+
+  /**
+   * Get a list of entity kinds from Software Catalog.
+   * @param param The request object
+   */
+  public listCatalogKind(
+    param: SoftwareCatalogApiListCatalogKindRequest = {},
+    options?: Configuration,
+  ): Promise<ListKindCatalogResponse> {
+    const requestContextPromise = this.requestFactory.listCatalogKind(
+      param.pageOffset,
+      param.pageLimit,
+      param.filterId,
+      param.filterName,
+      options,
+    );
+    return requestContextPromise.then((requestContext) => {
+      return this.configuration.httpApi
+        .send(requestContext)
+        .then((responseContext) => {
+          return this.responseProcessor.listCatalogKind(responseContext);
+        });
+    });
+  }
+
+  /**
+   * Provide a paginated version of listCatalogKind returning a generator with all the items.
+   */
+  public async *listCatalogKindWithPagination(
+    param: SoftwareCatalogApiListCatalogKindRequest = {},
+    options?: Configuration,
+  ): AsyncGenerator<KindData> {
+    let pageSize = 100;
+    if (param.pageLimit !== undefined) {
+      pageSize = param.pageLimit;
+    }
+    param.pageLimit = pageSize;
+    while (true) {
+      const requestContext = await this.requestFactory.listCatalogKind(
+        param.pageOffset,
+        param.pageLimit,
+        param.filterId,
+        param.filterName,
+        options,
+      );
+      const responseContext =
+        await this.configuration.httpApi.send(requestContext);
+
+      const response =
+        await this.responseProcessor.listCatalogKind(responseContext);
       const responseData = response.data;
       if (responseData === undefined) {
         break;
@@ -878,6 +1344,27 @@ export class SoftwareCatalogApi {
         .send(requestContext)
         .then((responseContext) => {
           return this.responseProcessor.upsertCatalogEntity(responseContext);
+        });
+    });
+  }
+
+  /**
+   * Create or update kinds in Software Catalog.
+   * @param param The request object
+   */
+  public upsertCatalogKind(
+    param: SoftwareCatalogApiUpsertCatalogKindRequest,
+    options?: Configuration,
+  ): Promise<UpsertCatalogKindResponse> {
+    const requestContextPromise = this.requestFactory.upsertCatalogKind(
+      param.body,
+      options,
+    );
+    return requestContextPromise.then((requestContext) => {
+      return this.configuration.httpApi
+        .send(requestContext)
+        .then((responseContext) => {
+          return this.responseProcessor.upsertCatalogKind(responseContext);
         });
     });
   }
