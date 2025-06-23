2427acea0cf4cc5a9d6c5cbe647ef5c3c077a8b3
ci.datadog-api-spec <packages@datadoghq.com>
2025-06-23 18:02:38 +0000

    Regenerate client from commit 9a201ac of spec repo

--- services/action_connection/src/v2/models/AppKeyRegistrationDataType.ts
+++
@@ -1,9 +0,0 @@
-import { UnparsedObject } from "@datadog/datadog-api-client";
-
-/**
- * The definition of `AppKeyRegistrationDataType` object.
- */
-export type AppKeyRegistrationDataType =
-  | typeof APP_KEY_REGISTRATION
-  | UnparsedObject;
-export const APP_KEY_REGISTRATION = "app_key_registration";
