0217799dd8723eb284580cc562c59a5edd962e26
ci.datadog-api-spec <packages@datadoghq.com>
2025-06-30 08:49:05 +0000

    Regenerate client from commit 3684903 of spec repo

--- private/bdd_runner/src/support/scenarios_model_mapping.ts
+++ private/bdd_runner/src/support/scenarios_model_mapping.ts
@@ -5771,6 +5771,10 @@ export const ScenariosModelMappings: { [key: string]: OperationMapping } = {
       type: "string",
       format: "",
     },
+    windowSeconds: {
+      type: "number",
+      format: "int64",
+    },
     operationResponseType: "MetricVolumesResponse",
   },
   "MetricsApi.V2.QueryScalarData": {
