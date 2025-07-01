5fa0d9cfaf2b70a9a2864d8ae59a4008d642ef5d
ci.datadog-api-spec <packages@datadoghq.com>
2025-07-01 19:42:47 +0000

    Regenerate client from commit 4a51a6c of spec repo

--- services/logs_indexes/src/v1/LogsIndexesApi.ts
+++ services/logs_indexes/src/v1/LogsIndexesApi.ts
@@ -240,7 +240,6 @@ export class LogsIndexesApiRequestFactory extends BaseAPIRequestFactory {
 
     // Apply auth methods
     applySecurityAuthentication(_config, requestContext, [
-      "AuthZ",
       "apiKeyAuth",
       "appKeyAuth",
     ]);
