940883d1f70229a98e87af5bc9fde090f56043c5
ci.datadog-api-spec <packages@datadoghq.com>
2025-07-02 13:36:20 +0000

    Regenerate client from commit c552525a of spec repo

---
+++ packages/datadog-api-client-v2/models/ObservabilityPipelineSocketSourceFramingBytesMethod.ts
@@ -0,0 +1,16 @@
+/**
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2020-Present Datadog, Inc.
+ */
+
+import { UnparsedObject } from "../../datadog-api-client-common/util";
+
+/**
+ * Byte frames are passed through as-is according to the underlying I/O boundaries (for example, split between messages or stream segments).
+ */
+
+export type ObservabilityPipelineSocketSourceFramingBytesMethod =
+  | typeof BYTES
+  | UnparsedObject;
+export const BYTES = "bytes";
