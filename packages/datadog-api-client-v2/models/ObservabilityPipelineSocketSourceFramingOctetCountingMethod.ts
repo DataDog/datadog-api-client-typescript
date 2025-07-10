a90e73ac1080b5c098e172d38ba2b16b954d9dea
ci.datadog-api-spec <packages@datadoghq.com>
2025-07-10 12:36:17 +0000

    Regenerate client from commit 911f8778 of spec repo

---
+++ packages/datadog-api-client-v2/models/ObservabilityPipelineSocketSourceFramingOctetCountingMethod.ts
@@ -0,0 +1,16 @@
+/**
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2020-Present Datadog, Inc.
+ */
+
+import { UnparsedObject } from "../../datadog-api-client-common/util";
+
+/**
+ * Byte frames according to the octet counting format as per RFC6587.
+ */
+
+export type ObservabilityPipelineSocketSourceFramingOctetCountingMethod =
+  | typeof OCTET_COUNTING
+  | UnparsedObject;
+export const OCTET_COUNTING = "octet_counting";
