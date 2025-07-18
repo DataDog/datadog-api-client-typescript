d0b41bd5029de9b29189a9da7de06092bae14db4
ci.datadog-api-spec <packages@datadoghq.com>
2025-07-18 10:22:42 +0000

    Regenerate client from commit 20279f4 of spec repo

--- services/software_catalog/src/v2/models/EntityV3APIVersion.ts
+++ services/software_catalog/src/v2/models/EntityV3APIVersion.ts
@@ -1,7 +1,15 @@
 import { UnparsedObject } from "@datadog/datadog-api-client";
 
 /**
- * The schema version of entity type. The field is known as schema-version in the previous version.
+ * The version of the schema data that was used to populate this entity's data. This could be via the API, Terraform, or YAML file in a repository. The field is known as schema-version in the previous version.
  */
-export type EntityV3APIVersion = typeof V3 | UnparsedObject;
+export type EntityV3APIVersion =
+  | typeof V3
+  | typeof V2_2
+  | typeof V2_1
+  | typeof V2
+  | UnparsedObject;
 export const V3 = "v3";
+export const V2_2 = "v2.2";
+export const V2_1 = "v2.1";
+export const V2 = "v2";
