f6b97ab892f72f83bca3d3314b63080d68f89070
ci.datadog-api-spec <packages@datadoghq.com>
2025-07-28 19:54:18 +0000

    Regenerate client from commit df31e44 of spec repo

--- services/restriction_policies/src/v2/RestrictionPoliciesApi.ts
+++ services/restriction_policies/src/v2/RestrictionPoliciesApi.ts
@@ -370,7 +370,7 @@ export class RestrictionPoliciesApiResponseProcessor {
 
 export interface RestrictionPoliciesApiDeleteRestrictionPolicyRequest {
   /**
-   * Identifier, formatted as `type:id`. Supported types: `connection`, `dashboard`, `integration-account`, `integration-service`, `integration-webhook`, `notebook`, `reference-table`, `security-rule`, `slo`, `workflow`, `app-builder-app`, `connection`, `connection-group`, `rum-application`, `cross-org-connection`, `spreadsheet`.
+   * Identifier, formatted as `type:id`. Supported types: `dashboard`, `integration-service`, `integration-webhook`, `notebook`, `reference-table`, `security-rule`, `slo`, `workflow`, `app-builder-app`, `connection`, `connection-group`, `rum-application`, `cross-org-connection`, `spreadsheet`.
    * @type string
    */
   resourceId: string;
@@ -378,7 +378,7 @@ export interface RestrictionPoliciesApiDeleteRestrictionPolicyRequest {
 
 export interface RestrictionPoliciesApiGetRestrictionPolicyRequest {
   /**
-   * Identifier, formatted as `type:id`. Supported types: `connection`, `dashboard`, `integration-account`, `integration-service`, `integration-webhook`, `notebook`, `reference-table`, `security-rule`, `slo`, `workflow`, `app-builder-app`, `connection`, `connection-group`, `rum-application`, `cross-org-connection`, `spreadsheet`.
+   * Identifier, formatted as `type:id`. Supported types: `dashboard`, `integration-service`, `integration-webhook`, `notebook`, `reference-table`, `security-rule`, `slo`, `workflow`, `app-builder-app`, `connection`, `connection-group`, `rum-application`, `cross-org-connection`, `spreadsheet`.
    * @type string
    */
   resourceId: string;
@@ -386,7 +386,7 @@ export interface RestrictionPoliciesApiGetRestrictionPolicyRequest {
 
 export interface RestrictionPoliciesApiUpdateRestrictionPolicyRequest {
   /**
-   * Identifier, formatted as `type:id`. Supported types: `connection`, `dashboard`, `integration-account`, `integration-service`, `integration-webhook`, `notebook`, `reference-table`, `security-rule`, `slo`, `workflow`, `app-builder-app`, `connection`, `connection-group`, `rum-application`, `cross-org-connection`, `spreadsheet`.
+   * Identifier, formatted as `type:id`. Supported types: `dashboard`, `integration-service`, `integration-webhook`, `notebook`, `reference-table`, `security-rule`, `slo`, `workflow`, `app-builder-app`, `connection`, `connection-group`, `rum-application`, `cross-org-connection`, `spreadsheet`.
    * @type string
    */
   resourceId: string;
@@ -472,7 +472,6 @@ export class RestrictionPoliciesApi {
    * #### Supported resources
    * Restriction policies can be applied to the following resources:
    * - Dashboards: `dashboard`
-   * - Integration Accounts: `integration-account`
    * - Integration Services: `integration-service`
    * - Integration Webhooks: `integration-webhook`
    * - Notebooks: `notebook`
@@ -496,7 +495,6 @@ export class RestrictionPoliciesApi {
    * Resource Type               | Supported Relations
    * ----------------------------|--------------------------
    * Dashboards                  | `viewer`, `editor`
-   * Integration Accounts        | `viewer`, `editor`
    * Integration Services        | `viewer`, `editor`
    * Integration Webhooks        | `viewer`, `editor`
    * Notebooks                   | `viewer`, `editor`
