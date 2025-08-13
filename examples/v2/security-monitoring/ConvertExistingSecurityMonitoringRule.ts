a6492e1d9fb8b09160f7e67473e79dad00a1c62c
ci.datadog-api-spec <packages@datadoghq.com>
2025-08-13 20:28:25 +0000

    Regenerate client from commit 8001cbb of spec repo

--- examples/v2/security-monitoring/ConvertExistingSecurityMonitoringRule.ts
+++ examples/v2/security-monitoring/ConvertExistingSecurityMonitoringRule.ts
@@ -7,12 +7,12 @@ import { client, v2 } from "@datadog/datadog-api-client";
 const configuration = client.createConfiguration();
 const apiInstance = new v2.SecurityMonitoringApi(configuration);
 
-// there is a valid "security_rule" in the system
-const SECURITY_RULE_ID = process.env.SECURITY_RULE_ID as string;
+// there is a valid "security_rule_hash" in the system
+const SECURITY_RULE_HASH_ID = process.env.SECURITY_RULE_HASH_ID as string;
 
 const params: v2.SecurityMonitoringApiConvertExistingSecurityMonitoringRuleRequest =
   {
-    ruleId: SECURITY_RULE_ID,
+    ruleId: SECURITY_RULE_HASH_ID,
   };
 
 apiInstance
