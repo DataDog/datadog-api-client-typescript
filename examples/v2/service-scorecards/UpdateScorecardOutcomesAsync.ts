a888fb1b88b3ee954a2dee0237740fde8407759f
ci.datadog-api-spec <packages@datadoghq.com>
2025-08-15 16:36:42 +0000

    Regenerate client from commit c9f30db of spec repo

---
+++ examples/v2/service-scorecards/UpdateScorecardOutcomesAsync.ts
@@ -0,0 +1,36 @@
+/**
+ * Update Scorecard outcomes asynchronously returns "Accepted" response
+ */
+
+import { client, v2 } from "@datadog/datadog-api-client";
+
+const configuration = client.createConfiguration();
+configuration.unstableOperations["v2.updateScorecardOutcomesAsync"] = true;
+const apiInstance = new v2.ServiceScorecardsApi(configuration);
+
+const params: v2.ServiceScorecardsApiUpdateScorecardOutcomesAsyncRequest = {
+  body: {
+    data: {
+      attributes: {
+        results: [
+          {
+            entityReference: "service:my-service",
+            remarks: `See: <a href="https://app.datadoghq.com/services">Services</a>`,
+            ruleId: "q8MQxk8TCqrHnWkx",
+            state: "pass",
+          },
+        ],
+      },
+      type: "batched-outcome",
+    },
+  },
+};
+
+apiInstance
+  .updateScorecardOutcomesAsync(params)
+  .then((data: any) => {
+    console.log(
+      "API called successfully. Returned data: " + JSON.stringify(data)
+    );
+  })
+  .catch((error: any) => console.error(error));
