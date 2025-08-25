9eca799a62fd7394d2e1ac6634a92f2af8dcb153
ci.datadog-api-spec <packages@datadoghq.com>
2025-08-25 12:55:16 +0000

    Regenerate client from commit 397f8fc of spec repo

---
+++ examples/v2/case-management-type/GetAllCaseTypes.ts
@@ -0,0 +1,17 @@
+/**
+ * Get all case types returns "OK" response
+ */
+
+import { client, v2 } from "@datadog/datadog-api-client";
+
+const configuration = client.createConfiguration();
+const apiInstance = new v2.CaseManagementTypeApi(configuration);
+
+apiInstance
+  .getAllCaseTypes()
+  .then((data: v2.CaseTypesResponse) => {
+    console.log(
+      "API called successfully. Returned data: " + JSON.stringify(data)
+    );
+  })
+  .catch((error: any) => console.error(error));
