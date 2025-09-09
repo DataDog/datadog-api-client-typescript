2c52d5c0b90be86ea666cf1a9a290a474862c7ab
ci.datadog-api-spec <packages@datadoghq.com>
2025-09-09 07:20:26 +0000

    Regenerate client from commit 5302b12 of spec repo

--- services/ci_visibility_pipelines/src/v2/CIVisibilityPipelinesApi.ts
+++ services/ci_visibility_pipelines/src/v2/CIVisibilityPipelinesApi.ts
@@ -620,6 +620,7 @@ export class CIVisibilityPipelinesApi {
    * Send your pipeline event to your Datadog platform over HTTP. For details about how pipeline executions are modeled and what execution types we support, see [Pipeline Data Model And Execution Types](https://docs.datadoghq.com/continuous_integration/guides/pipeline_data_model/).
    *
    * Pipeline events can be submitted with a timestamp that is up to 18 hours in the past.
+   * The duration between the event start and end times cannot exceed 1 year.
    * @param param The request object
    */
   public createCIAppPipelineEvent(
