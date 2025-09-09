fafb286a4cdb9e74aeecb30cbf094f97a24ecf81
ci.datadog-api-spec <packages@datadoghq.com>
2025-09-09 12:30:55 +0000

    Regenerate client from commit 834034f of spec repo

--- examples/v2/aws-logs-integration/ListAWSLogsServices.ts
+++ examples/v2/aws-logs-integration/ListAWSLogsServices.ts
@@ -5,7 +5,6 @@
 import { client, v2 } from "@datadog/datadog-api-client";
 
 const configuration = client.createConfiguration();
-configuration.unstableOperations["v2.listAWSLogsServices"] = true;
 const apiInstance = new v2.AWSLogsIntegrationApi(configuration);
 
 apiInstance
