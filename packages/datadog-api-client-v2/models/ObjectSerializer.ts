05723c50e7e0922f38c243b41fe4744bcd12e799
ci.datadog-api-spec <packages@datadoghq.com>
2025-09-16 10:55:04 +0000

    Regenerate client from commit 6876f24 of spec repo

--- packages/datadog-api-client-v2/models/ObjectSerializer.ts
+++ packages/datadog-api-client-v2/models/ObjectSerializer.ts
@@ -1394,6 +1394,7 @@ import { ObservabilityPipelineNewRelicDestination } from "./ObservabilityPipelin
 import { ObservabilityPipelineOcsfMapperProcessor } from "./ObservabilityPipelineOcsfMapperProcessor";
 import { ObservabilityPipelineOcsfMapperProcessorMapping } from "./ObservabilityPipelineOcsfMapperProcessorMapping";
 import { ObservabilityPipelineOpenSearchDestination } from "./ObservabilityPipelineOpenSearchDestination";
+import { ObservabilityPipelineOpentelemetrySource } from "./ObservabilityPipelineOpentelemetrySource";
 import { ObservabilityPipelineParseGrokProcessor } from "./ObservabilityPipelineParseGrokProcessor";
 import { ObservabilityPipelineParseGrokProcessorRule } from "./ObservabilityPipelineParseGrokProcessorRule";
 import { ObservabilityPipelineParseGrokProcessorRuleMatchRule } from "./ObservabilityPipelineParseGrokProcessorRuleMatchRule";
@@ -3110,6 +3111,7 @@ const enumsMap: { [key: string]: any[] } = {
     "Palo Alto Networks Firewall Traffic",
   ],
   ObservabilityPipelineOpenSearchDestinationType: ["opensearch"],
+  ObservabilityPipelineOpentelemetrySourceType: ["opentelemetry"],
   ObservabilityPipelineParseGrokProcessorType: ["parse_grok"],
   ObservabilityPipelineParseJSONProcessorType: ["parse_json"],
   ObservabilityPipelinePipelineKafkaSourceSaslMechanism: [
@@ -5380,6 +5382,8 @@ const typeMap: { [index: string]: any } = {
     ObservabilityPipelineOcsfMapperProcessorMapping,
   ObservabilityPipelineOpenSearchDestination:
     ObservabilityPipelineOpenSearchDestination,
+  ObservabilityPipelineOpentelemetrySource:
+    ObservabilityPipelineOpentelemetrySource,
   ObservabilityPipelineParseGrokProcessor:
     ObservabilityPipelineParseGrokProcessor,
   ObservabilityPipelineParseGrokProcessorRule:
@@ -6707,6 +6711,7 @@ const oneOfMap: { [index: string]: string[] } = {
     "ObservabilityPipelineGooglePubSubSource",
     "ObservabilityPipelineHttpClientSource",
     "ObservabilityPipelineLogstashSource",
+    "ObservabilityPipelineOpentelemetrySource",
     "ObservabilityPipelineSocketSource",
   ],
   ObservabilityPipelineMetricValue: [
