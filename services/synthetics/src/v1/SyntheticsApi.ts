ee6ccdfeee34d73d919420cedb250d89e71c93c5
ci.datadog-api-spec <packages@datadoghq.com>
2025-09-19 13:33:52 +0000

    Regenerate client from commit a26b725 of spec repo

--- services/synthetics/src/v1/SyntheticsApi.ts
+++ services/synthetics/src/v1/SyntheticsApi.ts
@@ -1352,7 +1352,6 @@ export class SyntheticsApiRequestFactory extends BaseAPIRequestFactory {
   public async searchTests(
     text?: string,
     includeFullConfig?: boolean,
-    searchSuites?: boolean,
     facetsOnly?: boolean,
     start?: number,
     count?: number,
@@ -1394,13 +1393,6 @@ export class SyntheticsApiRequestFactory extends BaseAPIRequestFactory {
       requestContext.setQueryParam(
         "include_full_config",
         serialize(includeFullConfig, TypingInfo, "boolean", ""),
-        "",
-      );
-    }
-    if (searchSuites !== undefined) {
-      requestContext.setQueryParam(
-        "search_suites",
-        serialize(searchSuites, TypingInfo, "boolean", ""),
         "",
       );
     }
@@ -4091,11 +4083,6 @@ export interface SyntheticsApiSearchTestsRequest {
    */
   includeFullConfig?: boolean;
   /**
-   * If true, returns suites instead of tests.
-   * @type boolean
-   */
-  searchSuites?: boolean;
-  /**
    * If true, return only facets instead of full test details.
    * @type boolean
    */
@@ -4814,7 +4801,7 @@ export class SyntheticsApi {
   }
 
   /**
-   * Search for Synthetic tests and Test Suites.
+   * Search for Synthetic tests.
    * @param param The request object
    */
   public searchTests(
@@ -4824,7 +4811,6 @@ export class SyntheticsApi {
     const requestContextPromise = this.requestFactory.searchTests(
       param.text,
       param.includeFullConfig,
-      param.searchSuites,
       param.facetsOnly,
       param.start,
       param.count,
