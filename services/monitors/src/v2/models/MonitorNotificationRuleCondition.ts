58df9ec3d11cfba31b8706bcf7f2b00cbc3d07e7
ci.datadog-api-spec <packages@datadoghq.com>
2025-09-26 02:00:34 +0000

    Regenerate client from commit 88fa4c2 of spec repo

---
+++ services/monitors/src/v2/models/MonitorNotificationRuleCondition.ts
@@ -0,0 +1,54 @@
+import { AttributeTypeMap } from "@datadog/datadog-api-client";
+
+/**
+ * Conditions for `conditional_recipients`.
+ */
+export class MonitorNotificationRuleCondition {
+  /**
+   * A list of recipients to notify. Uses the same format as the monitor `message` field. Must not start with an '@'.
+   */
+  "recipients": Array<string>;
+  /**
+   * The scope to which the monitor applied.
+   */
+  "scope": string;
+  /**
+   * A container for additional, undeclared properties.
+   * This is a holder for any undeclared properties as specified with
+   * the 'additionalProperties' keyword in the OAS document.
+   */
+  "additionalProperties"?: { [key: string]: any };
+  /**
+   * @ignore
+   */
+  "_unparsed"?: boolean;
+
+  /**
+   * @ignore
+   */
+  static readonly attributeTypeMap: AttributeTypeMap = {
+    recipients: {
+      baseName: "recipients",
+      type: "Array<string>",
+      required: true,
+    },
+    scope: {
+      baseName: "scope",
+      type: "string",
+      required: true,
+    },
+    additionalProperties: {
+      baseName: "additionalProperties",
+      type: "{ [key: string]: any; }",
+    },
+  };
+
+  /**
+   * @ignore
+   */
+  static getAttributeTypeMap(): AttributeTypeMap {
+    return MonitorNotificationRuleCondition.attributeTypeMap;
+  }
+
+  public constructor() {}
+}
