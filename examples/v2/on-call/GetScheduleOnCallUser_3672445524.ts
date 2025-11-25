883e06af09bf676680831e973f8c71cffb7aaab8
ci.datadog-api-spec <packages@datadoghq.com>
2025-11-25 17:47:46 +0000

    Regenerate client from commit 479ca66 of spec repo

--- examples/v2/on-call/GetScheduleOnCallUser_3672445524.ts
+++ examples/v2/on-call/GetScheduleOnCallUser_3672445524.ts
@@ -1,5 +1,5 @@
 /**
- * Get the scheduled on-call user returns "OK" response
+ * Get the schedule on-call user returns "OK" response
  */
 
 import { client, v2 } from "@datadog/datadog-api-client";
@@ -7,8 +7,11 @@ import { client, v2 } from "@datadog/datadog-api-client";
 const configuration = client.createConfiguration();
 const apiInstance = new v2.OnCallApi(configuration);
 
+// there is a valid "schedule" in the system
+const SCHEDULE_DATA_ID = process.env.SCHEDULE_DATA_ID as string;
+
 const params: v2.OnCallApiGetScheduleOnCallUserRequest = {
-  scheduleId: "3653d3c6-0c75-11ea-ad28-fb5701eabc7d",
+  scheduleId: SCHEDULE_DATA_ID,
 };
 
 apiInstance
