eb68efbf117a4c52d1c538a82b4dfa45ad904735
ci.datadog-api-spec <packages@datadoghq.com>
2025-12-02 17:52:39 +0000

    Regenerate client from commit 50c62af of spec repo

---
+++ examples/v1/monitors/CreateMonitor_3541766733.ts
@@ -0,0 +1,46 @@
+/**
+ * Create a monitor with assets returns "OK" response
+ */
+
+import { client, v1 } from "@datadog/datadog-api-client";
+
+const configuration = client.createConfiguration();
+const apiInstance = new v1.MonitorsApi(configuration);
+
+const params: v1.MonitorsApiCreateMonitorRequest = {
+  body: {
+    assets: [
+      {
+        category: "runbook",
+        name: "Monitor Runbook",
+        resourceKey: "12345",
+        resourceType: "notebook",
+        url: "/notebooks/12345",
+      },
+    ],
+    name: "Example-Monitor",
+    type: "metric alert",
+    query: "avg(current_1mo):avg:system.load.5{*} > 0.5",
+    message: "some message Notify: @hipchat-channel",
+    options: {
+      thresholds: {
+        critical: 0.5,
+      },
+      schedulingOptions: {
+        evaluationWindow: {
+          dayStarts: "04:00",
+          monthStarts: 1,
+        },
+      },
+    },
+  },
+};
+
+apiInstance
+  .createMonitor(params)
+  .then((data: v1.Monitor) => {
+    console.log(
+      "API called successfully. Returned data: " + JSON.stringify(data)
+    );
+  })
+  .catch((error: any) => console.error(error));
