d6996011e635f60047574a5646b436bf142d9e63
ci.datadog-api-spec <packages@datadoghq.com>
2025-12-23 10:59:48 +0000

    Regenerate client from commit 127da32 of spec repo

---
+++ services/observability_pipelines/src/v2/models/ObservabilityPipelineEnrichmentTableReferenceTable.ts
@@ -0,0 +1,70 @@
+import { AttributeTypeMap } from "@datadog/datadog-api-client";
+
+/**
+ * Uses a Datadog reference table to enrich logs.
+ */
+export class ObservabilityPipelineEnrichmentTableReferenceTable {
+  /**
+   * Environment variable name containing the application key used for reference table authentication.
+   */
+  "appKeyKey"?: string;
+  /**
+   * List of column names to include from the reference table. If not provided, all columns are included.
+   */
+  "columns"?: Array<string>;
+  /**
+   * Path to the field in the log event to match against the reference table.
+   */
+  "keyField": string;
+  /**
+   * The unique identifier of the reference table.
+   */
+  "tableId": string;
+  /**
+   * A container for additional, undeclared properties.
+   * This is a holder for any undeclared properties as specified with
+   * the 'additionalProperties' keyword in the OAS document.
+   */
+  "additionalProperties"?: { [key: string]: any };
+  /**
+   * @ignore
+   */
+  "_unparsed"?: boolean;
+
+  /**
+   * @ignore
+   */
+  static readonly attributeTypeMap: AttributeTypeMap = {
+    appKeyKey: {
+      baseName: "app_key_key",
+      type: "string",
+    },
+    columns: {
+      baseName: "columns",
+      type: "Array<string>",
+    },
+    keyField: {
+      baseName: "key_field",
+      type: "string",
+      required: true,
+    },
+    tableId: {
+      baseName: "table_id",
+      type: "string",
+      required: true,
+    },
+    additionalProperties: {
+      baseName: "additionalProperties",
+      type: "{ [key: string]: any; }",
+    },
+  };
+
+  /**
+   * @ignore
+   */
+  static getAttributeTypeMap(): AttributeTypeMap {
+    return ObservabilityPipelineEnrichmentTableReferenceTable.attributeTypeMap;
+  }
+
+  public constructor() {}
+}
