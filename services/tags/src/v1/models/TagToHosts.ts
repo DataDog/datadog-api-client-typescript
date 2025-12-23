3814f89cef4564ffbfb0b3657455a44983b47cfe
ci.datadog-api-spec <packages@datadoghq.com>
2025-12-23 20:09:16 +0000

    Regenerate client from commit 45ed7ff of spec repo

--- services/tags/src/v1/models/TagToHosts.ts
+++ services/tags/src/v1/models/TagToHosts.ts
@@ -1,11 +1,11 @@
 import { AttributeTypeMap } from "@datadog/datadog-api-client";
 
 /**
- * In this object, the key is the tag, the value is a list of host names that are reporting that tag.
+ * In this object, the key is the tag, and the value is a list of host names that are reporting that tag.
  */
 export class TagToHosts {
   /**
-   * A list of tags to apply to the host.
+   * A mapping of tags to host names
    */
   "tags"?: { [key: string]: Array<string> };
   /**
