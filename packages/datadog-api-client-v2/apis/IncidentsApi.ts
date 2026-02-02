ad32894252267e6dcb92982939f64b850181dcdf
ci.datadog-api-spec <packages@datadoghq.com>
2026-02-02 15:38:48 +0000

    Regenerate client from commit 99230b2 of spec repo

--- packages/datadog-api-client-v2/apis/IncidentsApi.ts
+++ packages/datadog-api-client-v2/apis/IncidentsApi.ts
@@ -6091,6 +6091,10 @@ export class IncidentsApi {
 
   /**
    * Create a postmortem attachment for an incident.
+   *
+   * The endpoint accepts markdown for notebooks created in Confluence or Google Docs.
+   * Postmortems created from notebooks need to be formatted using frontend notebook cells,
+   * in addition to markdown format.
    * @param param The request object
    */
   public createIncidentPostmortemAttachment(
