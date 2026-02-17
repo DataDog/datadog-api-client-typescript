e70940535d33539a2759c5d5ab83e20f47d11dea
ci.datadog-api-spec <packages@datadoghq.com>
2026-02-17 08:39:06 +0000

    Regenerate client from commit 9560e24 of spec repo

--- examples/v2/teams/UpdateTeamMembership.ts
+++ examples/v2/teams/UpdateTeamMembership.ts
@@ -1,11 +1,17 @@
 /**
- * Update a user's membership attributes on a team returns "Represents a user's association to a team" response
+ * Update a user's membership attributes on a team returns "OK" response
  */
 
 import { client, v2 } from "@datadog/datadog-api-client";
 
 const configuration = client.createConfiguration();
 const apiInstance = new v2.TeamsApi(configuration);
+
+// there is a valid "dd_team" in the system
+const DD_TEAM_DATA_ID = process.env.DD_TEAM_DATA_ID as string;
+
+// there is a valid "user" in the system
+const USER_DATA_ID = process.env.USER_DATA_ID as string;
 
 const params: v2.TeamsApiUpdateTeamMembershipRequest = {
   body: {
@@ -16,8 +22,8 @@ const params: v2.TeamsApiUpdateTeamMembershipRequest = {
       type: "team_memberships",
     },
   },
-  teamId: "team_id",
-  userId: "user_id",
+  teamId: DD_TEAM_DATA_ID,
+  userId: USER_DATA_ID,
 };
 
 apiInstance
