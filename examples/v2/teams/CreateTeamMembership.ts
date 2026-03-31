1f18f520cd5b162fa5f7959782687718f44fd907
ci.datadog-api-spec <packages@datadoghq.com>
2026-03-31 15:11:38 +0000

    Regenerate client from commit 9a344d9 of spec repo

--- examples/v2/teams/CreateTeamMembership.ts
+++ examples/v2/teams/CreateTeamMembership.ts
@@ -7,6 +7,12 @@ import { client, v2 } from "@datadog/datadog-api-client";
 const configuration = client.createConfiguration();
 const apiInstance = new v2.TeamsApi(configuration);
 
+// there is a valid "dd_team" in the system
+const DD_TEAM_DATA_ID = process.env.DD_TEAM_DATA_ID as string;
+
+// there is a valid "user" in the system
+const USER_DATA_ID = process.env.USER_DATA_ID as string;
+
 const params: v2.TeamsApiCreateTeamMembershipRequest = {
   body: {
     data: {
@@ -14,15 +20,9 @@ const params: v2.TeamsApiCreateTeamMembershipRequest = {
         role: "admin",
       },
       relationships: {
-        team: {
-          data: {
-            id: "d7e15d9d-d346-43da-81d8-3d9e71d9a5e9",
-            type: "team",
-          },
-        },
         user: {
           data: {
-            id: "b8626d7e-cedd-11eb-abf5-da7ad0900001",
+            id: USER_DATA_ID,
             type: "users",
           },
         },
@@ -30,7 +30,7 @@ const params: v2.TeamsApiCreateTeamMembershipRequest = {
       type: "team_memberships",
     },
   },
-  teamId: "team_id",
+  teamId: DD_TEAM_DATA_ID,
 };
 
 apiInstance
