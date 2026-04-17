40c3d6993b8ca19c1996e598ac70b8585236c65d
ci.datadog-api-spec <packages@datadoghq.com>
2026-04-17 09:15:57 +0000

    Regenerate client from commit 64bf902 of spec repo

--- packages/datadog-api-client-v2/models/IPAllowlistData.ts
+++ packages/datadog-api-client-v2/models/IPAllowlistData.ts
@@ -17,7 +17,7 @@ export class IPAllowlistData {
    */
   "attributes"?: IPAllowlistAttributes;
   /**
-   * The unique identifier of the org.
+   * The unique identifier of the organization.
    */
   "id"?: string;
   /**
