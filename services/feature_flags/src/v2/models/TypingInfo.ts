194c57d59f52ed954a05c3284b4163340defe489
ci.datadog-api-spec <packages@datadoghq.com>
2026-04-23 12:51:35 +0000

    Regenerate client from commit 35d655b of spec repo

--- services/feature_flags/src/v2/models/TypingInfo.ts
+++ services/feature_flags/src/v2/models/TypingInfo.ts
@@ -30,6 +30,9 @@ import { ExposureScheduleRequest } from "./ExposureScheduleRequest";
 import { FeatureFlag } from "./FeatureFlag";
 import { FeatureFlagAttributes } from "./FeatureFlagAttributes";
 import { FeatureFlagEnvironment } from "./FeatureFlagEnvironment";
+import { FeatureFlagEnvironmentListItem } from "./FeatureFlagEnvironmentListItem";
+import { FeatureFlagListItem } from "./FeatureFlagListItem";
+import { FeatureFlagListItemAttributes } from "./FeatureFlagListItemAttributes";
 import { FeatureFlagResponse } from "./FeatureFlagResponse";
 import { FeatureFlagsPaginationMeta } from "./FeatureFlagsPaginationMeta";
 import { FeatureFlagsPaginationMetaPage } from "./FeatureFlagsPaginationMetaPage";
@@ -112,6 +115,9 @@ export const TypingInfo: ModelTypingInfo = {
     FeatureFlag: FeatureFlag,
     FeatureFlagAttributes: FeatureFlagAttributes,
     FeatureFlagEnvironment: FeatureFlagEnvironment,
+    FeatureFlagEnvironmentListItem: FeatureFlagEnvironmentListItem,
+    FeatureFlagListItem: FeatureFlagListItem,
+    FeatureFlagListItemAttributes: FeatureFlagListItemAttributes,
     FeatureFlagResponse: FeatureFlagResponse,
     FeatureFlagsPaginationMeta: FeatureFlagsPaginationMeta,
     FeatureFlagsPaginationMetaPage: FeatureFlagsPaginationMetaPage,
