30b76f95c94c31c97dc28c2294b51d772325553c
ci.datadog-api-spec <packages@datadoghq.com>
2026-04-27 14:52:58 +0000

    Regenerate client from commit 16c48ca of spec repo

--- packages/datadog-api-client-v2/index.ts
+++ packages/datadog-api-client-v2/index.ts
@@ -118,11 +118,11 @@ export {
 } from "./apis/AuthNMappingsApi";
 
 export {
-  BitsAIApiGetInvestigationRequest,
-  BitsAIApiListInvestigationsRequest,
-  BitsAIApiTriggerInvestigationRequest,
-  BitsAIApi,
-} from "./apis/BitsAIApi";
+  BitsAISREApiGetInvestigationRequest,
+  BitsAISREApiListInvestigationsRequest,
+  BitsAISREApiTriggerInvestigationRequest,
+  BitsAISREApi,
+} from "./apis/BitsAISREApi";
 
 export {
   CIVisibilityPipelinesApiAggregateCIAppPipelineEventsRequest,
