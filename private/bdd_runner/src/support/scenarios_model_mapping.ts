9f050eaae6f97a55e87b69fc5d12cabdb48779a8
ci.datadog-api-spec <packages@datadoghq.com>
2026-05-11 15:43:35 +0000

    Regenerate client from commit 509db0a of spec repo

--- private/bdd_runner/src/support/scenarios_model_mapping.ts
+++ private/bdd_runner/src/support/scenarios_model_mapping.ts
@@ -6901,13 +6901,6 @@ export const ScenariosModelMappings: { [key: string]: OperationMapping } = {
     },
     operationResponseType: "MonitorDowntimeMatchResponse",
   },
-  "EmailTransportApi.V2.CreateEmailTransportWebhookIntake": {
-    body: {
-      type: "Array<TransportWebhookLog>",
-      format: "",
-    },
-    operationResponseType: "{}",
-  },
   "ErrorTrackingApi.V2.SearchIssues": {
     include: {
       type: "Array<SearchIssuesIncludeQueryParameterItem>",
