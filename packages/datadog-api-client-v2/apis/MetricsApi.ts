ecbd9f6ab1259ca3de83d2fbda691b28376cea41
ci.datadog-api-spec <packages@datadoghq.com>
2026-06-01 12:37:26 +0000

    Regenerate client from commit 0949fb2 of spec repo

--- packages/datadog-api-client-v2/apis/MetricsApi.ts
+++ packages/datadog-api-client-v2/apis/MetricsApi.ts
@@ -2450,7 +2450,7 @@ export class MetricsApi {
       for (const item of results) {
         yield item;
       }
-      if (results.length < pageSize) {
+      if (results.length === 0) {
         break;
       }
       const cursorMeta = response.meta;
--- packages/datadog-api-client-v2/apis/SpansApi.ts
+++ packages/datadog-api-client-v2/apis/SpansApi.ts
@@ -538,7 +538,7 @@ export class SpansApi {
       for (const item of results) {
         yield item;
       }
-      if (results.length < pageSize) {
+      if (results.length === 0) {
         break;
       }
       const cursorMeta = response.meta;
@@ -627,7 +627,7 @@ export class SpansApi {
       for (const item of results) {
         yield item;
       }
-      if (results.length < pageSize) {
+      if (results.length === 0) {
         break;
       }
       const cursorMeta = response.meta;
