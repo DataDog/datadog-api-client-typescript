db9ef386db2d79e751eac3f8e19e784afbc32178
ci.datadog-api-spec <packages@datadoghq.com>
2026-06-22 14:59:12 +0000

    Regenerate client from commit 0460aa0 of spec repo

--- services/security_monitoring/src/v2/models/SecurityMonitoringTerraformResourceType.ts
+++ services/security_monitoring/src/v2/models/SecurityMonitoringTerraformResourceType.ts
@@ -6,6 +6,10 @@ import { UnparsedObject } from "@datadog/datadog-api-client";
 export type SecurityMonitoringTerraformResourceType =
   | typeof SUPPRESSIONS
   | typeof CRITICAL_ASSETS
+  | typeof SECURITY_FILTERS
+  | typeof RULES
   | UnparsedObject;
 export const SUPPRESSIONS = "suppressions";
 export const CRITICAL_ASSETS = "critical_assets";
+export const SECURITY_FILTERS = "security_filters";
+export const RULES = "rules";
