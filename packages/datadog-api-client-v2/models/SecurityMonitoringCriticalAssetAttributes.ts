57ab70e9c577e3f74abd7e7442b76124cb6bf1d8
ci.datadog-api-spec <packages@datadoghq.com>
2026-06-30 13:46:50 +0000

    Regenerate client from commit 3bd06e0 of spec repo

--- packages/datadog-api-client-v2/models/SecurityMonitoringCriticalAssetAttributes.ts
+++ packages/datadog-api-client-v2/models/SecurityMonitoringCriticalAssetAttributes.ts
@@ -25,6 +25,10 @@ export class SecurityMonitoringCriticalAssetAttributes {
    */
   "creator"?: SecurityMonitoringUser;
   /**
+   * A description of the critical asset.
+   */
+  "description"?: string;
+  /**
    * Whether the critical asset is enabled.
    */
   "enabled"?: boolean;
@@ -90,6 +94,10 @@ export class SecurityMonitoringCriticalAssetAttributes {
     creator: {
       baseName: "creator",
       type: "SecurityMonitoringUser",
+    },
+    description: {
+      baseName: "description",
+      type: "string",
     },
     enabled: {
       baseName: "enabled",
