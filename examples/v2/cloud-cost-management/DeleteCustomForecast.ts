f6db5e741ea008585aa39b7b67c0dcae45218feb
ci.datadog-api-spec <packages@datadoghq.com>
2026-07-21 16:10:31 +0000

    Regenerate client from commit 025f88b of spec repo

--- examples/v2/cloud-cost-management/DeleteCustomForecast.ts
+++ examples/v2/cloud-cost-management/DeleteCustomForecast.ts
@@ -5,7 +5,6 @@
 import { client, v2 } from "@datadog/datadog-api-client";
 
 const configuration = client.createConfiguration();
-configuration.unstableOperations["v2.deleteCustomForecast"] = true;
 const apiInstance = new v2.CloudCostManagementApi(configuration);
 
 const params: v2.CloudCostManagementApiDeleteCustomForecastRequest = {
