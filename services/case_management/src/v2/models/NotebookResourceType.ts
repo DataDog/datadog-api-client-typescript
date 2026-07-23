5089913341d82daaa95778751345b9921e2d6792
ci.datadog-api-spec <packages@datadoghq.com>
2026-07-23 20:42:36 +0000

    Regenerate client from commit adcb573 of spec repo

--- services/case_management/src/v2/models/NotebookResourceType.ts
+++
@@ -1,7 +0,0 @@
-import { UnparsedObject } from "@datadog/datadog-api-client";
-
-/**
- * Notebook resource type
- */
-export type NotebookResourceType = typeof NOTEBOOK | UnparsedObject;
-export const NOTEBOOK = "notebook";
