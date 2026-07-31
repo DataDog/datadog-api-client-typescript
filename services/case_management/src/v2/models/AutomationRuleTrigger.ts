65cae9f54f26640c75278c43a66ffb35149cc2aa
ci.datadog-api-spec <packages@datadoghq.com>
2026-07-31 12:27:14 +0000

    Regenerate client from commit cdbb6f8 of spec repo

--- services/case_management/src/v2/models/AutomationRuleTrigger.ts
+++ services/case_management/src/v2/models/AutomationRuleTrigger.ts
@@ -8,7 +8,7 @@ import { AutomationRuleTriggerType } from "./AutomationRuleTriggerType";
  */
 export class AutomationRuleTrigger {
   /**
-   * Additional configuration for the trigger, dependent on the trigger type. For `status_transitioned` triggers, specify `from_status_name` and `to_status_name`. For `attribute_value_changed` triggers, specify `field` and `change_type`.
+   * Additional configuration for the trigger, dependent on the trigger type. For `STATUS_TRANSITIONED` triggers, specify `from_status_name` and `to_status_name`. For `ATTRIBUTE_VALUE_CHANGED` triggers, specify `field` and `change_type`.
    */
   "data"?: AutomationRuleTriggerData;
   /**
