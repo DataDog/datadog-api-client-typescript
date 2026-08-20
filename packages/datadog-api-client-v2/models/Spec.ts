5b2599e4ff0936618ab81ca1bc0407f64a550fa0
ci.datadog-api-spec <packages@datadoghq.com>
2026-08-20 17:15:25 +0000

    Regenerate client from commit 58be88f of spec repo

--- packages/datadog-api-client-v2/models/Spec.ts
+++ packages/datadog-api-client-v2/models/Spec.ts
@@ -29,7 +29,7 @@ export class Spec {
    */
   "handle"?: string;
   /**
-   * A list of input parameters for the workflow. These can be used as dynamic runtime values in your workflow.
+   * A list of input parameters for the workflow. Input parameters are available under the `Trigger` object and can be referenced in workflow steps using `{{ Trigger.<parameter_name> }}`.
    */
   "inputSchema"?: InputSchema;
   /**
