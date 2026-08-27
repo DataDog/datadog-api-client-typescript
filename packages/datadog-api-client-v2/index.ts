2a380270cf6552e460210443732a4221ed34b81c
ci.datadog-api-spec <packages@datadoghq.com>
2026-08-27 07:57:56 +0000

    Regenerate client from commit fb473a3 of spec repo

--- packages/datadog-api-client-v2/index.ts
+++ packages/datadog-api-client-v2/index.ts
@@ -3860,6 +3860,7 @@ export { DomainAllowlistResponse } from "./models/DomainAllowlistResponse";
 export { DomainAllowlistResponseData } from "./models/DomainAllowlistResponseData";
 export { DomainAllowlistResponseDataAttributes } from "./models/DomainAllowlistResponseDataAttributes";
 export { DomainAllowlistType } from "./models/DomainAllowlistType";
+export { DORADeploymentAveragedMetrics } from "./models/DORADeploymentAveragedMetrics";
 export { DORADeploymentFetchResponse } from "./models/DORADeploymentFetchResponse";
 export { DORADeploymentObject } from "./models/DORADeploymentObject";
 export { DORADeploymentObjectAttributes } from "./models/DORADeploymentObjectAttributes";
@@ -3875,6 +3876,7 @@ export { DORADeploymentPatchRequest } from "./models/DORADeploymentPatchRequest"
 export { DORADeploymentPatchRequestAttributes } from "./models/DORADeploymentPatchRequestAttributes";
 export { DORADeploymentPatchRequestData } from "./models/DORADeploymentPatchRequestData";
 export { DORADeploymentPatchRequestDataType } from "./models/DORADeploymentPatchRequestDataType";
+export { DORADeploymentRemediation } from "./models/DORADeploymentRemediation";
 export { DORADeploymentRequest } from "./models/DORADeploymentRequest";
 export { DORADeploymentRequestAttributes } from "./models/DORADeploymentRequestAttributes";
 export { DORADeploymentRequestData } from "./models/DORADeploymentRequestData";
