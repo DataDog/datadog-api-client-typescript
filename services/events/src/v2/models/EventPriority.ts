93e23f0cde02233df9e50e5b3abdfbed6a4c303b
ci.datadog-api-spec <packages@datadoghq.com>
2026-08-28 21:40:18 +0000

    Regenerate client from commit ad7bd2a of spec repo

--- services/events/src/v2/models/EventPriority.ts
+++ services/events/src/v2/models/EventPriority.ts
@@ -1,8 +1,22 @@
 import { UnparsedObject } from "@datadog/datadog-api-client";
 
 /**
- * The priority of the event's monitor. For example, `normal` or `low`.
+ * The priority of the event alert. Legacy events use `normal` or `low`.
+ * Alert events use `1` (highest priority) through `5` (lowest priority).
  */
-export type EventPriority = typeof NORMAL | typeof LOW | UnparsedObject;
+export type EventPriority =
+  | typeof NORMAL
+  | typeof LOW
+  | typeof PRIORITY_ONE
+  | typeof PRIORITY_TWO
+  | typeof PRIORITY_THREE
+  | typeof PRIORITY_FOUR
+  | typeof PRIORITY_FIVE
+  | UnparsedObject;
 export const NORMAL = "normal";
 export const LOW = "low";
+export const PRIORITY_ONE = "1";
+export const PRIORITY_TWO = "2";
+export const PRIORITY_THREE = "3";
+export const PRIORITY_FOUR = "4";
+export const PRIORITY_FIVE = "5";
