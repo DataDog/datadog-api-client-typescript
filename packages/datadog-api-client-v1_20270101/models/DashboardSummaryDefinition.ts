6c0c2c3a45a21d7c2ee8e991ba87c6fd0f2c4b51
ci.datadog-api-spec <packages@datadoghq.com>
2026-09-01 09:11:24 +0000

    Regenerate client from commit 3445017 of spec repo

---
+++ packages/datadog-api-client-v1_20270101/models/DashboardSummaryDefinition.ts
@@ -0,0 +1,169 @@
+/**
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2020-Present Datadog, Inc.
+ */
+import { Creator } from "../../datadog-api-client-v1/models/Creator";
+import { DashboardSummaryID } from "./DashboardSummaryID";
+
+import { AttributeTypeMap } from "../../datadog-api-client-common/util";
+
+/**
+ * Dashboard definition.
+ */
+export class DashboardSummaryDefinition {
+  /**
+   * Object describing the creator of the shared element.
+   */
+  "author"?: Creator;
+  /**
+   * Date of creation of the dashboard.
+   */
+  "created"?: Date;
+  /**
+   * URL to the icon of the dashboard.
+   */
+  "icon"?: string;
+  /**
+   * ID of the dashboard.
+   */
+  "id"?: DashboardSummaryID;
+  /**
+   * The short name of the integration.
+   */
+  "integrationId"?: string;
+  /**
+   * Whether the dashboard is in the favorites.
+   */
+  "isFavorite"?: boolean;
+  /**
+   * Whether the dashboard is read only.
+   */
+  "isReadOnly"?: boolean;
+  /**
+   * Whether the dashboard is publicly shared.
+   */
+  "isShared"?: boolean;
+  /**
+   * Date when the dashboard was last viewed.
+   */
+  "lastViewDate"?: string;
+  /**
+   * Date of last edition of the dashboard.
+   */
+  "modified"?: Date;
+  /**
+   * Popularity of the dashboard.
+   */
+  "popularity"?: number;
+  /**
+   * List of team names representing ownership of the dashboard.
+   */
+  "tags"?: Array<string>;
+  /**
+   * Title of the dashboard.
+   */
+  "title"?: string;
+  /**
+   * The type of the dashboard.
+   */
+  "type"?: string;
+  /**
+   * URL path to the dashboard.
+   */
+  "url"?: string;
+
+  /**
+   * A container for additional, undeclared properties.
+   * This is a holder for any undeclared properties as specified with
+   * the 'additionalProperties' keyword in the OAS document.
+   */
+  "additionalProperties"?: { [key: string]: any };
+
+  /**
+   * @ignore
+   */
+  "_unparsed"?: boolean;
+
+  /**
+   * @ignore
+   */
+  static readonly attributeTypeMap: AttributeTypeMap = {
+    author: {
+      baseName: "author",
+      type: "Creator",
+    },
+    created: {
+      baseName: "created",
+      type: "Date",
+      format: "date-time",
+    },
+    icon: {
+      baseName: "icon",
+      type: "string",
+    },
+    id: {
+      baseName: "id",
+      type: "DashboardSummaryID",
+    },
+    integrationId: {
+      baseName: "integration_id",
+      type: "string",
+    },
+    isFavorite: {
+      baseName: "is_favorite",
+      type: "boolean",
+    },
+    isReadOnly: {
+      baseName: "is_read_only",
+      type: "boolean",
+    },
+    isShared: {
+      baseName: "is_shared",
+      type: "boolean",
+    },
+    lastViewDate: {
+      baseName: "last_view_date",
+      type: "string",
+    },
+    modified: {
+      baseName: "modified",
+      type: "Date",
+      format: "date-time",
+    },
+    popularity: {
+      baseName: "popularity",
+      type: "number",
+      format: "int32",
+    },
+    tags: {
+      baseName: "tags",
+      type: "Array<string>",
+    },
+    title: {
+      baseName: "title",
+      type: "string",
+    },
+    type: {
+      baseName: "type",
+      type: "string",
+    },
+    url: {
+      baseName: "url",
+      type: "string",
+    },
+    additionalProperties: {
+      baseName: "additionalProperties",
+      type: "{ [key: string]: any; }",
+    },
+  };
+
+  /**
+   * @ignore
+   */
+  static getAttributeTypeMap(): AttributeTypeMap {
+    return DashboardSummaryDefinition.attributeTypeMap;
+  }
+
+  public constructor() {}
+}
