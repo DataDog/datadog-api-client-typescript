c7115da881cd821a6465e87445dd14f878d0030d
ci.datadog-api-spec <packages@datadoghq.com>
2026-09-10 17:24:46 +0000

    Regenerate client from commit c3f76df of spec repo

--- packages/datadog-api-client-v2/models/TicketCreationTarget.ts
+++ packages/datadog-api-client-v2/models/TicketCreationTarget.ts
@@ -13,6 +13,8 @@ import { UnparsedObject } from "../../datadog-api-client-common/util";
 export type TicketCreationTarget =
   | typeof JIRA
   | typeof CASE_MANAGEMENT
+  | typeof LINEAR
   | UnparsedObject;
 export const JIRA = "jira";
 export const CASE_MANAGEMENT = "case_management";
+export const LINEAR = "linear";
