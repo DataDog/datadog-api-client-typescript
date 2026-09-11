c186af5da62794da5e1b07facebeae968f728d80
ci.datadog-api-spec <packages@datadoghq.com>
2026-09-11 16:23:07 +0000

    Regenerate client from commit 13b767f of spec repo

--- examples/v2/oci-integration/GetTenancyConfigs.ts
+++ examples/v2/oci-integration/GetTenancyConfigs.ts
@@ -5,7 +5,6 @@
 import { client, v2 } from "@datadog/datadog-api-client";
 
 const configuration = client.createConfiguration();
-configuration.unstableOperations["v2.getTenancyConfigs"] = true;
 const apiInstance = new v2.OCIIntegrationApi(configuration);
 
 apiInstance
