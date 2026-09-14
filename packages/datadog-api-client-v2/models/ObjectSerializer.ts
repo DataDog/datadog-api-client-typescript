56543802e4018816bbd5285b54cd6a030db4d49d
ci.datadog-api-spec <packages@datadoghq.com>
2026-09-14 08:03:59 +0000

    Regenerate client from commit 9bc79cc of spec repo

--- packages/datadog-api-client-v2/models/ObjectSerializer.ts
+++ packages/datadog-api-client-v2/models/ObjectSerializer.ts
@@ -1529,6 +1529,8 @@ import { DeploymentRelationship } from "./DeploymentRelationship";
 import { DeploymentRelationshipData } from "./DeploymentRelationshipData";
 import { DeploymentRuleOptionsFaultyDeploymentDetection } from "./DeploymentRuleOptionsFaultyDeploymentDetection";
 import { DeploymentRuleOptionsMonitor } from "./DeploymentRuleOptionsMonitor";
+import { DeploymentRuleOptionsMonitorId } from "./DeploymentRuleOptionsMonitorId";
+import { DeploymentRuleOptionsMonitorIds } from "./DeploymentRuleOptionsMonitorIds";
 import { DeploymentRuleResponse } from "./DeploymentRuleResponse";
 import { DeploymentRuleResponseData } from "./DeploymentRuleResponseData";
 import { DeploymentRuleResponseDataAttributes } from "./DeploymentRuleResponseDataAttributes";
@@ -11211,6 +11213,8 @@ const typeMap: { [index: string]: any } = {
   DeploymentRuleOptionsFaultyDeploymentDetection:
     DeploymentRuleOptionsFaultyDeploymentDetection,
   DeploymentRuleOptionsMonitor: DeploymentRuleOptionsMonitor,
+  DeploymentRuleOptionsMonitorId: DeploymentRuleOptionsMonitorId,
+  DeploymentRuleOptionsMonitorIds: DeploymentRuleOptionsMonitorIds,
   DeploymentRuleResponse: DeploymentRuleResponse,
   DeploymentRuleResponseData: DeploymentRuleResponseData,
   DeploymentRuleResponseDataAttributes: DeploymentRuleResponseDataAttributes,
@@ -17061,6 +17065,7 @@ const oneOfMap: { [index: string]: string[] } = {
   DeploymentRulesOptions: [
     "DeploymentRuleOptionsFaultyDeploymentDetection",
     "DeploymentRuleOptionsMonitor",
+    "DeploymentRuleOptionsMonitorIds",
   ],
   DowntimeMonitorIdentifier: [
     "DowntimeMonitorIdentifierId",
