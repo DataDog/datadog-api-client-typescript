7a4f799a49a6e31bdbe0e7d0db5449d821a66a34
ci.datadog-api-spec <packages@datadoghq.com>
2026-09-14 10:28:44 +0000

    Regenerate client from commit 4612bd5 of spec repo

--- packages/datadog-api-client-v2/models/ObjectSerializer.ts
+++ packages/datadog-api-client-v2/models/ObjectSerializer.ts
@@ -157,6 +157,7 @@ import { AggregatedWaterfallRequestAttributes } from "./AggregatedWaterfallReque
 import { AggregatedWaterfallRequestData } from "./AggregatedWaterfallRequestData";
 import { AggregatedWaterfallResponse } from "./AggregatedWaterfallResponse";
 import { AggregatedWaterfallResponseAttributes } from "./AggregatedWaterfallResponseAttributes";
+import { AggregatedWaterfallResponseAttributesCriteria } from "./AggregatedWaterfallResponseAttributesCriteria";
 import { AggregatedWaterfallResponseData } from "./AggregatedWaterfallResponseData";
 import { AiCustomRuleItem } from "./AiCustomRuleItem";
 import { AiCustomRuleRequest } from "./AiCustomRuleRequest";
@@ -9579,6 +9580,8 @@ const typeMap: { [index: string]: any } = {
   AggregatedWaterfallRequestData: AggregatedWaterfallRequestData,
   AggregatedWaterfallResponse: AggregatedWaterfallResponse,
   AggregatedWaterfallResponseAttributes: AggregatedWaterfallResponseAttributes,
+  AggregatedWaterfallResponseAttributesCriteria:
+    AggregatedWaterfallResponseAttributesCriteria,
   AggregatedWaterfallResponseData: AggregatedWaterfallResponseData,
   AiCustomRuleItem: AiCustomRuleItem,
   AiCustomRuleRequest: AiCustomRuleRequest,
