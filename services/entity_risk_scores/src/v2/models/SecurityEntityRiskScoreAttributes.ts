d24dd340ebdde3b5016fbfea896b864ff195574f
ci.datadog-api-spec <packages@datadoghq.com>
2026-09-16 15:09:36 +0000

    Regenerate client from commit fded47e of spec repo

--- services/entity_risk_scores/src/v2/models/SecurityEntityRiskScoreAttributes.ts
+++ services/entity_risk_scores/src/v2/models/SecurityEntityRiskScoreAttributes.ts
@@ -69,7 +69,7 @@ export class SecurityEntityRiskScoreAttributes {
    */
   "severity": SecurityEntityRiskScoreAttributesSeverity;
   /**
-   * Number of security signals detected for this entity.
+   * Number of risk-contributing security signals detected for this entity.
    */
   "signalsDetected": number;
   /**
