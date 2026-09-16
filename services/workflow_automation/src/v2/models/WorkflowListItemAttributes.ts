ea2399a3935d34a52d2cf79f2d3af3935e098eff
ci.datadog-api-spec <packages@datadoghq.com>
2026-09-16 07:53:23 +0000

    Regenerate client from commit ba3b721 of spec repo

--- services/workflow_automation/src/v2/models/WorkflowListItemAttributes.ts
+++ services/workflow_automation/src/v2/models/WorkflowListItemAttributes.ts
@@ -28,6 +28,10 @@ export class WorkflowListItemAttributes {
    */
   "runAsUserMode"?: WorkflowRunAsUserMode;
   /**
+   * Whether the workflow requires sensitive privileges to run. Only the workflow owner can update this field. This allows it to run actions that use [Execution Policies](https://docs.datadoghq.com/actions/private_actions/execution_policies/).
+   */
+  "sensitivePrivileges"?: boolean;
+  /**
    * A complete Workflow Automation definition, including its triggers, steps, and connections.
    */
   "spec"?: Spec;
@@ -75,6 +79,10 @@ export class WorkflowListItemAttributes {
     runAsUserMode: {
       baseName: "runAsUserMode",
       type: "WorkflowRunAsUserMode",
+    },
+    sensitivePrivileges: {
+      baseName: "sensitivePrivileges",
+      type: "boolean",
     },
     spec: {
       baseName: "spec",
