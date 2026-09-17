4544e3b2bb0849f772a77a2c98a171fdcf564fb0
ci.datadog-api-spec <packages@datadoghq.com>
2026-09-17 15:39:44 +0000

    Regenerate client from commit 45b46d0 of spec repo

---
+++ packages/datadog-api-client-v2/models/RecommendationsFilterRequestData.ts
@@ -0,0 +1,73 @@
+/**
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2020-Present Datadog, Inc.
+ */
+import { RecommendationsFilterRequestDataAttributes } from "./RecommendationsFilterRequestDataAttributes";
+import { RecommendationsFilterRequestDataType } from "./RecommendationsFilterRequestDataType";
+
+import { AttributeTypeMap } from "../../datadog-api-client-common/util";
+
+/**
+ * JSON:API resource containing the cost recommendations filter. This legacy search contract
+ * uses the resource ID for the filter expression rather than as a persistent resource identifier.
+ */
+export class RecommendationsFilterRequestData {
+  /**
+   * Attributes used to filter and sort cost recommendations.
+   */
+  "attributes"?: RecommendationsFilterRequestDataAttributes;
+  /**
+   * Filter expression applied to the recommendations. The server treats an omitted ID as `*`
+   * and returns all recommendations.
+   */
+  "id"?: string;
+  /**
+   * Legacy JSON:API resource type required by the cost recommendations search decoder.
+   */
+  "type": RecommendationsFilterRequestDataType;
+
+  /**
+   * A container for additional, undeclared properties.
+   * This is a holder for any undeclared properties as specified with
+   * the 'additionalProperties' keyword in the OAS document.
+   */
+  "additionalProperties"?: { [key: string]: any };
+
+  /**
+   * @ignore
+   */
+  "_unparsed"?: boolean;
+
+  /**
+   * @ignore
+   */
+  static readonly attributeTypeMap: AttributeTypeMap = {
+    attributes: {
+      baseName: "attributes",
+      type: "RecommendationsFilterRequestDataAttributes",
+    },
+    id: {
+      baseName: "id",
+      type: "string",
+    },
+    type: {
+      baseName: "type",
+      type: "RecommendationsFilterRequestDataType",
+      required: true,
+    },
+    additionalProperties: {
+      baseName: "additionalProperties",
+      type: "{ [key: string]: any; }",
+    },
+  };
+
+  /**
+   * @ignore
+   */
+  static getAttributeTypeMap(): AttributeTypeMap {
+    return RecommendationsFilterRequestData.attributeTypeMap;
+  }
+
+  public constructor() {}
+}
