c8e35d1ef1240a6a10070adba64a4bbf2f674299
ci.datadog-api-spec <packages@datadoghq.com>
2026-09-18 13:28:10 +0000

    Regenerate client from commit 60c67b3 of spec repo

--- services/teams/src/v2/TeamsApi.ts
+++ services/teams/src/v2/TeamsApi.ts
@@ -5314,6 +5314,7 @@ export class TeamsApi {
   /**
    * Get all teams.
    * Can be used to search for teams using the `filter[keyword]` and `filter[me]` query parameters.
+   * Results are paginated.
    * @param param The request object
    */
   public listTeams(
