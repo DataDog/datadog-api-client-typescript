c63aed1b2c2ab85768147938e4ac4cdc6a4be912
ci.datadog-api-spec <packages@datadoghq.com>
2026-09-18 14:22:35 +0000

    Regenerate client from commit f23e221 of spec repo

--- services/logs/src/v1/LogsApi.ts
+++ services/logs/src/v1/LogsApi.ts
@@ -108,6 +108,9 @@ export class LogsApiRequestFactory extends BaseAPIRequestFactory {
     if (body === null || body === undefined) {
       throw new RequiredError("body", "submitLog");
     }
+    if (contentEncoding === undefined) {
+      contentEncoding = "gzip";
+    }
 
     // Path Params
     const localVarPath = "/v1/input";
