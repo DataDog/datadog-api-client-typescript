e0b5b56087df2b64abdced7273522be250a61678
ci.datadog-api-spec <packages@datadoghq.com>
2026-09-18 18:29:55 +0000

    Regenerate client from commit 6bb6903 of spec repo

---
+++ services/terraform_state_files/src/index.ts
@@ -0,0 +1,3 @@
+export * as v2 from "./v2";
+
+export { TerraformStateFilesApi as TerraformStateFilesApiV2 } from "./v2/TerraformStateFilesApi";
---
+++ services/terraform_state_files/src/v2/index.ts
@@ -0,0 +1,26 @@
+export {
+  TerraformStateFilesApiCreateTerraformBackendSyncConfigRequest,
+  TerraformStateFilesApiDeleteTerraformBackendSyncConfigRequest,
+  TerraformStateFilesApiListTerraformBackendSyncConfigsRequest,
+  TerraformStateFilesApiUpdateTerraformBackendSyncConfigRequest,
+  TerraformStateFilesApi,
+} from "./TerraformStateFilesApi";
+
+export { APIErrorResponse } from "./models/APIErrorResponse";
+export { JSONAPIErrorItem } from "./models/JSONAPIErrorItem";
+export { JSONAPIErrorItemSource } from "./models/JSONAPIErrorItemSource";
+export { JSONAPIErrorResponse } from "./models/JSONAPIErrorResponse";
+export { TerraformBackendAttributes } from "./models/TerraformBackendAttributes";
+export { TerraformBackendBucket } from "./models/TerraformBackendBucket";
+export { TerraformBackendCreateAttributes } from "./models/TerraformBackendCreateAttributes";
+export { TerraformBackendCreateData } from "./models/TerraformBackendCreateData";
+export { TerraformBackendCreateRequest } from "./models/TerraformBackendCreateRequest";
+export { TerraformBackendData } from "./models/TerraformBackendData";
+export { TerraformBackendKind } from "./models/TerraformBackendKind";
+export { TerraformBackendListResponse } from "./models/TerraformBackendListResponse";
+export { TerraformBackendResponse } from "./models/TerraformBackendResponse";
+export { TerraformBackendSyncStatus } from "./models/TerraformBackendSyncStatus";
+export { TerraformBackendType } from "./models/TerraformBackendType";
+export { TerraformBackendUpdateAttributes } from "./models/TerraformBackendUpdateAttributes";
+export { TerraformBackendUpdateData } from "./models/TerraformBackendUpdateData";
+export { TerraformBackendUpdateRequest } from "./models/TerraformBackendUpdateRequest";
