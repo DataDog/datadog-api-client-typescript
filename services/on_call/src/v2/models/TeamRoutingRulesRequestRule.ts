1dc7cf26b79cf7340a4893071d0904d755d57821
ci.datadog-api-spec <packages@datadoghq.com>
2026-09-24 11:44:51 +0000

    Regenerate client from commit 20014b8 of spec repo

--- services/on_call/src/v2/models/TeamRoutingRulesRequestRule.ts
+++ services/on_call/src/v2/models/TeamRoutingRulesRequestRule.ts
@@ -13,6 +13,11 @@ export class TeamRoutingRulesRequestRule {
    */
   "actions"?: Array<RoutingRuleAction>;
   /**
+   * Specifies the unique identifier of an existing routing rule to update.
+   * If omitted, a new routing rule is created.
+   */
+  "id"?: string;
+  /**
    * Identifies the policy to be applied when this routing rule matches.
    */
   "policyId"?: string;
@@ -46,6 +51,10 @@ export class TeamRoutingRulesRequestRule {
     actions: {
       baseName: "actions",
       type: "Array<RoutingRuleAction>",
+    },
+    id: {
+      baseName: "id",
+      type: "string",
     },
     policyId: {
       baseName: "policy_id",
